ba8de76cd47c35e18710dc87b09920ba255297ab
Giuseppe Valente <giuseppe@elastic.co>
2017-03-30 16:56:52 -0700

    Generate stubs for methods

--- generator.go
+++ generator.go
@@ -1,18 +1,74 @@
-package generator
+package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"strings"
+	"text/template"
 
 	"github.com/golang/glog"
 )
 
 const (
-	apiDir = "spec/elasticsearch/rest-api-spec/src/main/resources/rest-api-spec/api"
+	specDir         = "spec/elasticsearch/rest-api-spec/src/main/resources/rest-api-spec/api"
+	specExt         = ".json"
+	apiDir          = "api"
+	templatesDir    = "templates"
+	templateFile    = "method.tmpl"
+	defaultTypeName = "client"
 )
 
-func unmarshal(filename string) (map[string]interface{}, error) {
+type method struct {
+	TypeVar    string
+	TypeName   string
+	MethodName string
+}
+
+func executeTemplate(spec map[string]interface{}, outputDir string) error {
+	if len(spec) > 1 {
+		return fmt.Errorf("Each API spec is expected to contain a single API (found %d)", len(spec))
+	}
+	var api string
+	for k := range spec {
+		api = k
+	}
+	typeName := defaultTypeName
+	apiParts := strings.Split(api, ".")
+	switch len(apiParts) {
+	case 1:
+		api = apiParts[0]
+	case 2:
+		typeName = apiParts[0]
+		api = apiParts[1]
+	default:
+		return fmt.Errorf("Unexpected API format: %s", api)
+	}
+	m := &method{
+		TypeVar:    string(typeName[0]),
+		TypeName:   typeName,
+		MethodName: api,
+	}
+	t, err := template.ParseFiles(filepath.Join(templatesDir, templateFile))
+	if err != nil {
+		return fmt.Errorf("Failed to parse template in %q: %s", templateFile, err)
+	}
+	goFile, err := os.Create(filepath.Join(outputDir, api) + ".go")
+	if err != nil {
+		return err
+	}
+	defer goFile.Close()
+	err = t.Execute(goFile, m)
+	if err != nil {
+		return fmt.Errorf("Failed to execute template in %q: %s", templateFile, err)
+	}
+	return err
+}
+
+func unmarshalSpec(filename string) (map[string]interface{}, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -23,12 +79,19 @@ func unmarshal(filename string) (map[string]interface{}, error) {
 }
 
 func main() {
-	files, err := ioutil.ReadDir(apiDir)
+	flag.Parse()
+	files, err := ioutil.ReadDir(specDir)
 	if err != nil {
 		glog.Fatal(err)
 	}
 	for _, file := range files {
-		if _, err = unmarshal(filepath.Join(apiDir, file.Name())); err != nil {
+		spec, err := unmarshalSpec(filepath.Join(specDir, file.Name()))
+		if err != nil {
+			glog.Error(err)
+			continue
+		}
+		err = executeTemplate(spec, apiDir)
+		if err != nil {
 			glog.Error(err)
 			continue
 		}
--- generator_test.go
+++ generator_test.go
@@ -1,6 +1,7 @@
-package generator
+package main
 
 import (
+	"io/ioutil"
 	"path/filepath"
 	"testing"
 
@@ -10,7 +11,7 @@ import (
 func TestUnmarshal(t *testing.T) {
 	api := "indices.create"
 	filename := api + ".json"
-	spec, err := unmarshal(filepath.Join(apiDir, filename))
+	spec, err := unmarshalSpec(filepath.Join(specDir, filename))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -18,5 +19,26 @@ func TestUnmarshal(t *testing.T) {
 	methods := spec["indices.create"].(map[string]interface{})["methods"].([]interface{})
 	if len(methods) != 1 || methods[0].(string) != expectedMethod {
 		glog.Fatalf("Unexected methods: %s (expected %q)", methods, expectedMethod)
+	}
+}
+
+func TestExecuteTemplate(t *testing.T) {
+	api := "index"
+	spec := map[string]interface{}{api: map[string]interface{}{}}
+	apiDir, err := ioutil.TempDir("", "api")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = executeTemplate(spec, apiDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedCode := "func (c *client) index() {}\n"
+	code, err := ioutil.ReadFile(filepath.Join(apiDir, api) + ".go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(code) != expectedCode {
+		t.Fatalf("Expected the generation of %q, got %q", expectedCode, code)
 	}
 }
