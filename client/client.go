8ae316c57da53f43b2ae065496a84d58b5d7b9da
Giuseppe Valente <giuseppe@elastic.co>
2017-04-24 21:41:06 -0700

    Add example

--- client/client.go
+++ client/client.go
@@ -1,5 +1,17 @@
 // Package client is the starting point to establish a connection to Elasticsearch, as well as to invoke any of the
 // available APIs.
+//
+// Here's a quick example:
+//	c := client.New(client.WithHosts([]string{"https://elasticseach:9200"}))
+//	body := map[string]interface{}{
+//		"query": map[string]interface{}{
+//			"term": map[string]interface{}{
+//				"user": "kimchy",
+//			},
+//		},
+//	}
+//	resp, err := c.Search(body)
+//	// ...
 package client
 
 import (
