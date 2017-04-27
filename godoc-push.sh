d9f0cf6efb9868340f34c4d72c97b1678daaf2bf
Giuseppe Valente <giuseppe@elastic.co>
2017-04-27 10:27:12 -0700

    Replace localhost links

--- godoc-push.sh
+++ godoc-push.sh
@@ -19,6 +19,7 @@ until curl -sf http://localhost:$PORT; do sleep 2; done
 rm -rf godoc
 wget -r -np -N -E -p -k http://localhost:$PORT/pkg/github.com/elastic/goelasticsearch -e robots=off || true
 mv localhost:$PORT godoc
+grep -rl localhost:6060 godoc/ | xargs sed -i "" 's/localhost:6060/employees.org\/~giusva\/godoc/g'
 ssh $HOST "rm -rfv $DIR/godoc"
 scp -r godoc $HOST:$DIR
 killall godoc
