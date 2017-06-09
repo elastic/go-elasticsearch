d7061c530504f69e6bd5189e351ff5bb5a8b5373
Giuseppe Valente <giuseppe@elastic.co>
2017-06-09 16:42:26 +0200

    Handle method response in its own scope

--- generator/test/testcase_test.go
+++ generator/test/testcase_test.go
@@ -59,14 +59,14 @@ func TestDo(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	expectedCode := `indexResp, err := Index("test-weird-index-中文", "weird.type", map[string]interface{}{
-		}, WithID("1"))
-	if err != nil {
+	expectedCode := `if indexResp, err := Index("test-weird-index-中文", "weird.type", map[string]interface{}{
+		}, WithID("1")); err != nil {
 		t.Fatal(err)
-	}
-	body, err = indexResp.DecodeBody()
-	if err != nil {
-		t.Fatal(err)
+	} else {
+		body, err := indexResp.DecodeBody()
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 `
 	if d := common.Diff(t, expectedCode, s); len(d) > 0 {
@@ -98,15 +98,15 @@ func TestDoWithMissingParam(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	expectedCode := `deleteAliasResp, err := DeleteAlias(nil, []string{
+	expectedCode := `if deleteAliasResp, err := DeleteAlias(nil, []string{
 		"alias1",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	body, err = deleteAliasResp.DecodeBody()
-	if err != nil {
-		t.Fatal(err)
+	} else {
+		body, err := deleteAliasResp.DecodeBody()
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 `
 	if d := common.Diff(t, expectedCode, s); len(d) > 0 {
