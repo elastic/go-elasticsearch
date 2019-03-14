91987e80eba8e5b7e72fb0c9cf0c1c775a729270
Karel Minarik <karel.minarik@gmail.com>
2019-03-14 17:52:53 +0100

    CI: Add missing pattern when unzipping files from Elasticsearch archive in Jenkins test script

--- .jenkins/tests.sh
+++ .jenkins/tests.sh
@@ -11,7 +11,7 @@ echo -e "\033[1m>>>>> Downloading Elasticsearch repository @ $ES_BUILD_HASH...\0
 echo "curl -sSL -o elasticsearch-$ES_BUILD_HASH.zip https://github.com/elastic/elasticsearch/archive/$ES_BUILD_HASH.zip" && \
 (rm -rf tmp/elasticsearch) || true && (mkdir -p tmp || true) && \
 curl --retry 3 -sSL -o elasticsearch-$ES_BUILD_HASH.zip https://github.com/elastic/elasticsearch/archive/$ES_BUILD_HASH.zip && \
-unzip -q -o elasticsearch-$ES_BUILD_HASH.zip '*.json' '*.yml' -d tmp && \
+unzip -q -o elasticsearch-$ES_BUILD_HASH.zip '*.properties' '*.json' '*.yml' -d tmp && \
 mv tmp/elasticsearch-$ES_BUILD_HASH* tmp/elasticsearch && \
 echo -e "\033[1m>>>>> Generating the API registry\033[0m" && \
 (cd internal/cmd/generate && ELASTICSEARCH_BUILD_HASH=$ES_BUILD_HASH PACKAGE_PATH=$PWD/../../../esapi go generate -v ./... 2> /dev/null) && \
