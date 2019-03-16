c7dbbb6ef673cfbd47d24ab7acaa45e75fe2b17b
Karel Minarik <karel.minarik@gmail.com>
2019-03-16 10:48:10 +0100

    API: Add `formatDuration()` helper function
    This patch adds a helper method, `formatDuration()`, which properly converts a Go `time.Duration`
    value into the format accepted by Elasticsearch time units; see:
    https://www.elastic.co/guide/en/elasticsearch/reference/current/common-options.html#time-units.
    
    The generated code will use the function like this:
    
        if r.Timeout != 0 {
        	params["timeout"] = formatDuration(r.Timeout)
        }
    
    An integration test has been added as well.
    
    Related: #27, #25
    Closes: #36

--- esapi/esapi_internal_test.go
+++ esapi/esapi_internal_test.go
@@ -4,6 +4,7 @@ package esapi
 
 import (
 	"testing"
+	"time"
 )
 
 func TestAPIHelpers(t *testing.T) {
@@ -23,6 +24,30 @@ func TestAPIHelpers(t *testing.T) {
 		v := IntPtr(0)
 		if v == nil || *v != 0 {
 			t.Errorf("Expected 0, got: %v", v)
+		}
+	})
+
+	t.Run("FormatDuration", func(t *testing.T) {
+		var tt = []struct {
+			duration time.Duration
+			expected string
+		}{
+			{1 * time.Nanosecond, "1nanos"},
+			{100 * time.Nanosecond, "100nanos"},
+			{1 * time.Microsecond, "1000nanos"},
+			{1 * time.Millisecond, "1ms"},
+			{100 * time.Millisecond, "100ms"},
+			{1 * time.Minute, "60000ms"},
+			{10 * time.Minute, "600000ms"},
+			{1 * time.Hour, "3600000ms"},
+			{10 * time.Hour, "36000000ms"},
+		}
+
+		for _, tc := range tt {
+			actual := formatDuration(tc.duration)
+			if actual != tc.expected {
+				t.Errorf("Unexpected output: got=%s, want=%s", actual, tc.expected)
+			}
 		}
 	})
 }
