2cfbe3cd71962d4acf2b6a8203381799db72a636
Karel Minarik <karel.minarik@gmail.com>
2019-03-16 21:30:16 +0100

    Examples: Parsing JSON with "mailru/easyjson" and "tidwall/gjson"
    This patch ads examples of using the <github.com/mailru/easyjson> and <github.com/tidwall/gjson>
    packages for a more efficient decoding of Elasticsearch responses than provided by the standard library.
    
    To run the examples:
    
        make test
    
    To run the benchmarks:
    
        make bench
    
    (cherry picked from commit 956397d70b3d52d816e0e5e4b0a0181cd3da44d8)

---
+++ _examples/encoding/model/response.go
@@ -0,0 +1,40 @@
+//go:generate easyjson -all -snake_case $GOFILE
+
+package model
+
+type ErrorResponse struct {
+	Info *ErrorInfo `json:"error,omitempty"`
+}
+
+type ErrorInfo struct {
+	RootCause []*ErrorInfo
+	Type      string
+	Reason    string
+	Phase     string
+}
+
+type IndexResponse struct {
+	Index   string `json:"_index"`
+	ID      string `json:"_id"`
+	Version int    `json:"_version"`
+	Result  string
+}
+
+type SearchResponse struct {
+	Took int64
+	Hits struct {
+		Total struct {
+			Value int64
+		}
+		Hits []*SearchHit
+	}
+}
+
+type SearchHit struct {
+	Score   float64 `json:"_score"`
+	Index   string  `json:"_index"`
+	Type    string  `json:"_type"`
+	Version int64   `json:"_version,omitempty"`
+
+	Source Article `json:"_source"`
+}
