1cb4133cb29f38bbd1582c082f09769a206bf20c
Karel Minarik <karel.minarik@gmail.com>
2019-03-30 16:58:16 +0100

    Move the version definition to a separate internal package
    (cherry picked from commit 31f105e8db021ebaaa34f517a1355db140a99550)

--- esapi/version.go
+++
@@ -1,5 +0,0 @@
-package esapi
-
-// Version returns the package version as a string.
-//
-const Version = "6.7.0-SNAPSHOT"
---
+++ internal/version/version.go
@@ -0,0 +1,5 @@
+package version
+
+// Client returns the client version as a string.
+//
+const Client = "6.7.0-SNAPSHOT"
