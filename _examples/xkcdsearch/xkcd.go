f7c44bce8c969b2e7af12535b4273d7ffade20ec
Karel Minarik <karel.minarik@gmail.com>
2019-04-09 09:10:13 +0200

    Remove package import comments

--- _examples/xkcdsearch/xkcd.go
+++ _examples/xkcdsearch/xkcd.go
@@ -1,4 +1,4 @@
-package xkcdsearch // import "github.com/elastic/go-elasticsearch/v8/_examples/xkcdsearch"
+package xkcdsearch
 
 import (
 	"strings"
--- elasticsearch.go
+++ elasticsearch.go
@@ -1,4 +1,4 @@
-package elasticsearch // import "github.com/elastic/go-elasticsearch/v8"
+package elasticsearch
 
 import (
 	"errors"
