82fc17d38648bf652241ea16a64ccb8ab85138f6
Karel Minarik <karel.minarik@gmail.com>
2019-05-10 08:49:06 +0200

    Fix JSONReader integration with client
    This patch updates the esapi.newRequest() function to handle body passed as esutil.JSONReader properly.
    
    It makes the esutil.JSONReader exported, and renames the constructor function to esutil.NewJSONReader().
    
    It also adds proper integration tests for esutil.JSONReader.

---
+++ esutil/json_reader_integration_test.go
@@ -0,0 +1,68 @@
+// +build integration
+
+package esutil_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"github.com/elastic/go-elasticsearch/v8/esutil"
+)
+
+func TestJSONReaderIntegration(t *testing.T) {
+	t.Run("Index and search", func(t *testing.T) {
+		var (
+			res *esapi.Response
+			err error
+		)
+
+		es, err := elasticsearch.NewDefaultClient()
+		if err != nil {
+			t.Fatalf("Error creating the client: %s\n", err)
+		}
+
+		es.Indices.Delete([]string{"test"})
+
+		doc := struct {
+			Title string `json:"title"`
+		}{Title: "Foo Bar"}
+
+		res, err = es.Index("test", esutil.NewJSONReader(&doc), es.Index.WithRefresh("true"))
+		if err != nil {
+			t.Fatalf("Error getting response: %s", err)
+		}
+		defer res.Body.Close()
+
+		if res.IsError() {
+			t.Fatalf("Error response: %s", res.String())
+		}
+
+		query := map[string]interface{}{
+			"query": map[string]interface{}{
+				"match": map[string]interface{}{
+					"title": "foo",
+				},
+			},
+		}
+
+		res, err = es.Search(
+			es.Search.WithIndex("test"),
+			es.Search.WithBody(esutil.NewJSONReader(&query)),
+			es.Search.WithPretty(),
+		)
+		if err != nil {
+			t.Fatalf("Error getting response: %s", err)
+		}
+		defer res.Body.Close()
+
+		if res.IsError() {
+			t.Errorf("Error response: %s", res)
+		}
+
+		if !strings.Contains(res.String(), "Foo Bar") {
+			t.Errorf("Unexpected response: %s", res)
+		}
+	})
+}
