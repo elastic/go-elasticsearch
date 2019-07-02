b280f6824bc1c90d02d8a3baf42fd6d5df005807
Karel Minarik <karel.minarik@gmail.com>
2019-07-02 14:48:00 +0200

    API: Add integration test for the custom request headers
    (cherry picked from commit 844bced0393b158381246c3d4ef793ecfd438dc1)

--- esapi/esapi_integration_test.go
+++ esapi/esapi_integration_test.go
@@ -5,6 +5,7 @@ package esapi_test
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -18,6 +19,7 @@ func TestAPI(t *testing.T) {
 			t.Fatalf("Error creating the client: %s\n", err)
 		}
 
+		es.Cluster.Health(es.Cluster.Health.WithWaitForStatus("yellow"))
 		res, err := es.Search(es.Search.WithTimeout(500 * time.Millisecond))
 		if err != nil {
 			t.Fatalf("Error getting the response: %s\n", err)
@@ -34,5 +36,26 @@ func TestAPI(t *testing.T) {
 			t.Fatalf("Error parsing the response: %s\n", err)
 		}
 		fmt.Printf("took=%vms\n", d["took"])
+	})
+
+	t.Run("Headers", func(t *testing.T) {
+		es, err := elasticsearch.NewDefaultClient()
+		if err != nil {
+			t.Fatalf("Error creating the client: %s\n", err)
+		}
+
+		res, err := es.Info(es.Info.WithHeader(map[string]string{"Accept": "application/yaml"}))
+		if err != nil {
+			t.Fatalf("Error getting the response: %s\n", err)
+		}
+		defer res.Body.Close()
+
+		if res.IsError() {
+			t.Fatalf("Error response: %s", res.String())
+		}
+
+		if !strings.HasPrefix(res.String(), "[200 OK] ---") {
+			t.Errorf("Unexpected response body: doesn't start with '[200 OK] ---'; %s", res.String())
+		}
 	})
 }
