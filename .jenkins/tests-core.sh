091b2a535043b986ea7afedd5044b9746f085429
Karel Minarik <karel.minarik@gmail.com>
2019-07-04 18:51:12 +0200

    CI: Update the "core" test runner
    (cherry picked from commit 0e2ee8d7ae49759f2cf949a2b1dc07ba1da6c766)

--- .jenkins/tests-core.sh
+++ .jenkins/tests-core.sh
@@ -15,11 +15,11 @@ docker exec --workdir=/go-elasticsearch/internal/cmd/generate --env PACKAGE_PATH
 
 echo -e "\033[1m>>>>> Generating the test files\033[0m"
 
-time docker exec --tty --workdir=/go-elasticsearch/internal/cmd/generate go-elasticsearch go run main.go tests --output '/go-elasticsearch/esapi/test' --input '/elasticsearch-source/elasticsearch/rest-api-spec/src/main/resources/rest-api-spec/test/**/*.y*ml'
+time docker exec --tty --workdir=/go-elasticsearch/internal/cmd/generate go-elasticsearch go run main.go apitests --output '/go-elasticsearch/esapi/test' --input '/elasticsearch-source/elasticsearch/rest-api-spec/src/main/resources/rest-api-spec/test/**/*.y*ml'
 
 echo -e "\033[1m>>>>> Running the tests\033[0m"
 
-time docker exec --tty --workdir=/go-elasticsearch/esapi/test go-elasticsearch /bin/sh -c 'gotestsum --format=short-verbose --junitfile=$WORKSPACE/TEST-integration-api-junit.xml -- -tags=integration -timeout=1h ./...'
+time docker exec --tty --workdir=/go-elasticsearch/esapi/test go-elasticsearch /bin/sh -c 'gotestsum --format=short-verbose --junitfile=$WORKSPACE/TEST-integration-api-junit.xml -- -tags=integration -timeout=1h *_test.go'
 status=$?
 
 exit $status
