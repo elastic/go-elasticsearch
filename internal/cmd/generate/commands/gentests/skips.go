2533b053f7d265928c8ff6d03b4b78c81f3e499d
Karel Minarik <karel.minarik@gmail.com>
2019-07-04 16:40:18 +0200

    Generator: Tests: Updates, fixes and improvements
    * Rename the command to "apitests"
    * Add support for the X-Pack API type
    * Add support for running the tests against a cluster with SSL and authentication
    * Fix the incorrect setup/teardown functions (pass the *testing.T pointer)
    * Add stacktrace to error response log
    * Update the Core test setup
    * Add the X-Pack test setup
    * Add support for "transform_and_set" clause in REST tests
    * Fix incorrect values for duration
    * Fix incorrect Test.BaseFilename()
    * Update the list of skipped tests
    
    (cherry picked from commit f9f9e32a04ac7c495a5dbcf7f14c7ee5d5fc4f59)

--- internal/cmd/generate/commands/gentests/skips.go
+++ internal/cmd/generate/commands/gentests/skips.go
@@ -19,6 +19,14 @@ func init() {
 var skipFiles = []string{
 	"update/85_fields_meta.yml",            // Uses non-existing API property
 	"update/86_fields_meta_with_types.yml", // --||--
+
+	"ml/jobs_get_result_buckets.yml",    // Passes string value to int variable
+	"ml/jobs_get_result_categories.yml", // --||--
+	"ml/set_upgrade_mode.yml",           // --||--
+
+	"ml/evaluate_data_frame.yml", // Floats as map keys
+
+	"watcher/stats/10_basic.yml", // Sets "emit_stacktraces" as string ("true"), not bool
 }
 
 // TODO: Comments into descriptions for `Skip()`
@@ -82,4 +90,84 @@ search/issue9606.yml:
 bulk/80_cas.yml:
 bulk/81_cas_with_types.yml:
 
+# ----- X-Pack ----------------------------------------------------------------
+
+# Stash in body
+api_key/10_basic.yml:
+  - Test invalidate api key
+
+# Changing password locks out tests
+change_password/10_basic.yml:
+  - Test user changing their own password
+
+# Missing refreshes in the test
+data_frame/transforms_start_stop.yml:
+ml/index_layout.yml:
+
+# More QA tests than API tests
+data_frame/transforms_stats.yml:
+  - Test get multiple transform stats
+  - Test get transform stats on missing transform
+  - Test get multiple transform stats where one does not have a task
+
+# Invalid license makes subsequent tests fail
+license/20_put_license.yml:
+
+# Test tries to match on map from body, but Go keys are not sorted
+ml/jobs_crud.yml:
+  - Test job with rules
+
+# Test gets stuck every time
+ml/jobs_get_stats.yml:
+
+# # status_exception, Cannot process data because job [post-data-job] does not have a corresponding autodetect process
+# # resource_already_exists_exception, task with id {job-post-data-job} already exist
+# ml/post_data.yml:
+
+# Possible bad test setup, Cannot open job [start-stop-datafeed-job] because it has already been opened
+# resource_already_exists_exception, task with id {job-start-stop-datafeed-job-foo-2} already exist
+ml/start_stop_datafeed.yml:
+  - Test start datafeed when persistent task allocation disabled
+
+# Indexing step doesn't appear to work (getting total.hits=0)
+monitoring/bulk/10_basic.yml:
+  - Bulk indexing of monitoring data on closed indices should throw an export exception
+# Indexing step doesn't appear to work (getting total.hits=0)
+monitoring/bulk/20_privileges.yml:
+  - Monitoring Bulk API
+
+# Test tries to match on whole body, but map keys are unstable in Go
+rollup/security_tests.yml:
+
+# TEMPORARY: Missing 'body: { indices: "test_index" }' payload, TODO: PR
+snapshot/10_basic.yml:
+  - Create a source only snapshot and then restore it
+
+# illegal_argument_exception: Provided password hash uses [NOOP] but the configured hashing algorithm is [BCRYPT]
+users/10_basic.yml:
+  - Test put user with password hash
+
+# Slash in index name is not escaped (BUG)
+security/authz/13_index_datemath.yml:
+  - Test indexing documents with datemath, when permitted
+
+# Test looks for "testnode.crt", but "ca.crt" is returned first
+ssl/10_basic.yml:
+  - Test get SSL certificates
+
+# class org.elasticsearch.xpack.vectors.query.VectorScriptDocValues$DenseVectorScriptDocValues cannot be cast to class org.elasticsearch.xpack.vectors.query.VectorScriptDocValues$SparseVectorScriptDocValues ...
+vectors/30_sparse_vector_basic.yml:
+  - Dot Product
+# java.lang.IllegalArgumentException: No field found for [my_dense_vector] in mapping
+vectors/40_sparse_vector_special_cases.yml:
+  - Vectors of different dimensions and data types
+  - Dimensions can be sorted differently
+  - Distance functions for documents missing vector field should return 0
+
+# Cannot connect to Docker IP
+watcher/execute_watch/60_http_input.yml:
+
+# Test tries to match on "tagline", which requires "human=false", which doesn't work in the Go API.
+# Also test does too much within a single test, so has to be disabled as whole, unfortunately.
+xpack/15_basic.yml:
 `
