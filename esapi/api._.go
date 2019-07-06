2f5307822f9df9eec78fad16d60e652fdaa12a88
Karel Minarik <karel.minarik@gmail.com>
2019-07-06 10:19:00 +0200

    API: Update the API constructor for Elasticsearch 5.x (fe7575a32e2)

--- esapi/api._.go
+++ esapi/api._.go
@@ -1,4 +1,4 @@
-// Code generated from specification version 5.6.15 (fe7575a32e2|v5.6.15): DO NOT EDIT
+// Code generated from specification version 5.6.15 (fe7575a32e2): DO NOT EDIT
 
 package esapi
 
@@ -14,46 +14,50 @@ type API struct {
 	Snapshot *Snapshot
 	Tasks    *Tasks
 
-	Bulk                 Bulk
-	ClearScroll          ClearScroll
-	Count                Count
-	CountPercolate       CountPercolate
-	Create               Create
-	Delete               Delete
-	DeleteByQuery        DeleteByQuery
-	DeleteScript         DeleteScript
-	DeleteTemplate       DeleteTemplate
-	Exists               Exists
-	ExistsSource         ExistsSource
-	Explain              Explain
-	FieldCaps            FieldCaps
-	FieldStats           FieldStats
-	Get                  Get
-	GetScript            GetScript
-	GetSource            GetSource
-	GetTemplate          GetTemplate
-	Index                Index
-	Info                 Info
-	Mget                 Mget
-	Mpercolate           Mpercolate
-	Msearch              Msearch
-	MsearchTemplate      MsearchTemplate
-	Mtermvectors         Mtermvectors
-	Percolate            Percolate
-	Ping                 Ping
-	PutScript            PutScript
-	PutTemplate          PutTemplate
-	Reindex              Reindex
-	ReindexRethrottle    ReindexRethrottle
-	RenderSearchTemplate RenderSearchTemplate
-	Scroll               Scroll
-	Search               Search
-	SearchShards         SearchShards
-	SearchTemplate       SearchTemplate
-	Suggest              Suggest
-	Termvectors          Termvectors
-	Update               Update
-	UpdateByQuery        UpdateByQuery
+	Bulk                    Bulk
+	ClearScroll             ClearScroll
+	CountPercolate          CountPercolate
+	Count                   Count
+	Create                  Create
+	DeleteByQuery           DeleteByQuery
+	DeleteByQueryRethrottle DeleteByQueryRethrottle
+	Delete                  Delete
+	DeleteScript            DeleteScript
+	DeleteTemplate          DeleteTemplate
+	Exists                  Exists
+	ExistsSource            ExistsSource
+	Explain                 Explain
+	FieldCaps               FieldCaps
+	FieldStats              FieldStats
+	Get                     Get
+	GetScript               GetScript
+	GetSource               GetSource
+	GetTemplate             GetTemplate
+	Index                   Index
+	Info                    Info
+	Mget                    Mget
+	Mpercolate              Mpercolate
+	Msearch                 Msearch
+	MsearchTemplate         MsearchTemplate
+	Mtermvectors            Mtermvectors
+	Percolate               Percolate
+	Ping                    Ping
+	PutScript               PutScript
+	PutTemplate             PutTemplate
+	RankEval                RankEval
+	Reindex                 Reindex
+	ReindexRethrottle       ReindexRethrottle
+	RenderSearchTemplate    RenderSearchTemplate
+	ScriptsPainlessExecute  ScriptsPainlessExecute
+	Scroll                  Scroll
+	Search                  Search
+	SearchShards            SearchShards
+	SearchTemplate          SearchTemplate
+	Suggest                 Suggest
+	Termvectors             Termvectors
+	UpdateByQuery           UpdateByQuery
+	UpdateByQueryRethrottle UpdateByQueryRethrottle
+	Update                  Update
 }
 
 // Cat contains the Cat APIs
@@ -95,42 +99,43 @@ type Cluster struct {
 
 // Indices contains the Indices APIs
 type Indices struct {
-	Analyze         IndicesAnalyze
-	ClearCache      IndicesClearCache
-	Close           IndicesClose
-	Create          IndicesCreate
-	Delete          IndicesDelete
-	DeleteAlias     IndicesDeleteAlias
-	DeleteTemplate  IndicesDeleteTemplate
-	Exists          IndicesExists
-	ExistsAlias     IndicesExistsAlias
-	ExistsTemplate  IndicesExistsTemplate
-	ExistsType      IndicesExistsType
-	Flush           IndicesFlush
-	FlushSynced     IndicesFlushSynced
-	Forcemerge      IndicesForcemerge
-	Get             IndicesGet
-	GetAlias        IndicesGetAlias
-	GetFieldMapping IndicesGetFieldMapping
-	GetMapping      IndicesGetMapping
-	GetSettings     IndicesGetSettings
-	GetTemplate     IndicesGetTemplate
-	GetUpgrade      IndicesGetUpgrade
-	Open            IndicesOpen
-	PutAlias        IndicesPutAlias
-	PutMapping      IndicesPutMapping
-	PutSettings     IndicesPutSettings
-	PutTemplate     IndicesPutTemplate
-	Recovery        IndicesRecovery
-	Refresh         IndicesRefresh
-	Rollover        IndicesRollover
-	Segments        IndicesSegments
-	ShardStores     IndicesShardStores
-	Shrink          IndicesShrink
-	Stats           IndicesStats
-	UpdateAliases   IndicesUpdateAliases
-	Upgrade         IndicesUpgrade
-	ValidateQuery   IndicesValidateQuery
+	Analyze            IndicesAnalyze
+	ClearCache         IndicesClearCache
+	Close              IndicesClose
+	Create             IndicesCreate
+	DeleteAlias        IndicesDeleteAlias
+	Delete             IndicesDelete
+	DeleteTemplate     IndicesDeleteTemplate
+	ExistsAlias        IndicesExistsAlias
+	ExistsDocumentType IndicesExistsDocumentType
+	Exists             IndicesExists
+	ExistsTemplate     IndicesExistsTemplate
+	Flush              IndicesFlush
+	FlushSynced        IndicesFlushSynced
+	Forcemerge         IndicesForcemerge
+	GetAlias           IndicesGetAlias
+	GetFieldMapping    IndicesGetFieldMapping
+	GetMapping         IndicesGetMapping
+	Get                IndicesGet
+	GetSettings        IndicesGetSettings
+	GetTemplate        IndicesGetTemplate
+	GetUpgrade         IndicesGetUpgrade
+	Open               IndicesOpen
+	PutAlias           IndicesPutAlias
+	PutMapping         IndicesPutMapping
+	PutSettings        IndicesPutSettings
+	PutTemplate        IndicesPutTemplate
+	Recovery           IndicesRecovery
+	Refresh            IndicesRefresh
+	Rollover           IndicesRollover
+	Segments           IndicesSegments
+	ShardStores        IndicesShardStores
+	Shrink             IndicesShrink
+	Split              IndicesSplit
+	Stats              IndicesStats
+	UpdateAliases      IndicesUpdateAliases
+	Upgrade            IndicesUpgrade
+	ValidateQuery      IndicesValidateQuery
 }
 
 // Ingest contains the Ingest APIs
@@ -144,9 +149,11 @@ type Ingest struct {
 
 // Nodes contains the Nodes APIs
 type Nodes struct {
-	HotThreads NodesHotThreads
-	Info       NodesInfo
-	Stats      NodesStats
+	HotThreads           NodesHotThreads
+	Info                 NodesInfo
+	ReloadSecureSettings NodesReloadSecureSettings
+	Stats                NodesStats
+	Usage                NodesUsage
 }
 
 // Remote contains the Remote APIs
@@ -155,12 +162,12 @@ type Remote struct {
 
 // Snapshot contains the Snapshot APIs
 type Snapshot struct {
-	Create           SnapshotCreate
 	CreateRepository SnapshotCreateRepository
-	Delete           SnapshotDelete
+	Create           SnapshotCreate
 	DeleteRepository SnapshotDeleteRepository
-	Get              SnapshotGet
+	Delete           SnapshotDelete
 	GetRepository    SnapshotGetRepository
+	Get              SnapshotGet
 	Restore          SnapshotRestore
 	Status           SnapshotStatus
 	VerifyRepository SnapshotVerifyRepository
@@ -177,46 +184,50 @@ type Tasks struct {
 //
 func New(t Transport) *API {
 	return &API{
-		Bulk:                 newBulkFunc(t),
-		ClearScroll:          newClearScrollFunc(t),
-		Count:                newCountFunc(t),
-		CountPercolate:       newCountPercolateFunc(t),
-		Create:               newCreateFunc(t),
-		Delete:               newDeleteFunc(t),
-		DeleteByQuery:        newDeleteByQueryFunc(t),
-		DeleteScript:         newDeleteScriptFunc(t),
-		DeleteTemplate:       newDeleteTemplateFunc(t),
-		Exists:               newExistsFunc(t),
-		ExistsSource:         newExistsSourceFunc(t),
-		Explain:              newExplainFunc(t),
-		FieldCaps:            newFieldCapsFunc(t),
-		FieldStats:           newFieldStatsFunc(t),
-		Get:                  newGetFunc(t),
-		GetScript:            newGetScriptFunc(t),
-		GetSource:            newGetSourceFunc(t),
-		GetTemplate:          newGetTemplateFunc(t),
-		Index:                newIndexFunc(t),
-		Info:                 newInfoFunc(t),
-		Mget:                 newMgetFunc(t),
-		Mpercolate:           newMpercolateFunc(t),
-		Msearch:              newMsearchFunc(t),
-		MsearchTemplate:      newMsearchTemplateFunc(t),
-		Mtermvectors:         newMtermvectorsFunc(t),
-		Percolate:            newPercolateFunc(t),
-		Ping:                 newPingFunc(t),
-		PutScript:            newPutScriptFunc(t),
-		PutTemplate:          newPutTemplateFunc(t),
-		Reindex:              newReindexFunc(t),
-		ReindexRethrottle:    newReindexRethrottleFunc(t),
-		RenderSearchTemplate: newRenderSearchTemplateFunc(t),
-		Scroll:               newScrollFunc(t),
-		Search:               newSearchFunc(t),
-		SearchShards:         newSearchShardsFunc(t),
-		SearchTemplate:       newSearchTemplateFunc(t),
-		Suggest:              newSuggestFunc(t),
-		Termvectors:          newTermvectorsFunc(t),
-		Update:               newUpdateFunc(t),
-		UpdateByQuery:        newUpdateByQueryFunc(t),
+		Bulk:                    newBulkFunc(t),
+		ClearScroll:             newClearScrollFunc(t),
+		CountPercolate:          newCountPercolateFunc(t),
+		Count:                   newCountFunc(t),
+		Create:                  newCreateFunc(t),
+		DeleteByQuery:           newDeleteByQueryFunc(t),
+		DeleteByQueryRethrottle: newDeleteByQueryRethrottleFunc(t),
+		Delete:                  newDeleteFunc(t),
+		DeleteScript:            newDeleteScriptFunc(t),
+		DeleteTemplate:          newDeleteTemplateFunc(t),
+		Exists:                  newExistsFunc(t),
+		ExistsSource:            newExistsSourceFunc(t),
+		Explain:                 newExplainFunc(t),
+		FieldCaps:               newFieldCapsFunc(t),
+		FieldStats:              newFieldStatsFunc(t),
+		Get:                     newGetFunc(t),
+		GetScript:               newGetScriptFunc(t),
+		GetSource:               newGetSourceFunc(t),
+		GetTemplate:             newGetTemplateFunc(t),
+		Index:                   newIndexFunc(t),
+		Info:                    newInfoFunc(t),
+		Mget:                    newMgetFunc(t),
+		Mpercolate:              newMpercolateFunc(t),
+		Msearch:                 newMsearchFunc(t),
+		MsearchTemplate:         newMsearchTemplateFunc(t),
+		Mtermvectors:            newMtermvectorsFunc(t),
+		Percolate:               newPercolateFunc(t),
+		Ping:                    newPingFunc(t),
+		PutScript:               newPutScriptFunc(t),
+		PutTemplate:             newPutTemplateFunc(t),
+		RankEval:                newRankEvalFunc(t),
+		Reindex:                 newReindexFunc(t),
+		ReindexRethrottle:       newReindexRethrottleFunc(t),
+		RenderSearchTemplate:    newRenderSearchTemplateFunc(t),
+		ScriptsPainlessExecute:  newScriptsPainlessExecuteFunc(t),
+		Scroll:                  newScrollFunc(t),
+		Search:                  newSearchFunc(t),
+		SearchShards:            newSearchShardsFunc(t),
+		SearchTemplate:          newSearchTemplateFunc(t),
+		Suggest:                 newSuggestFunc(t),
+		Termvectors:             newTermvectorsFunc(t),
+		UpdateByQuery:           newUpdateByQueryFunc(t),
+		UpdateByQueryRethrottle: newUpdateByQueryRethrottleFunc(t),
+		Update:                  newUpdateFunc(t),
 		Cat: &Cat{
 			Aliases:      newCatAliasesFunc(t),
 			Allocation:   newCatAllocationFunc(t),
@@ -251,42 +262,43 @@ func New(t Transport) *API {
 			Stats:             newClusterStatsFunc(t),
 		},
 		Indices: &Indices{
-			Analyze:         newIndicesAnalyzeFunc(t),
-			ClearCache:      newIndicesClearCacheFunc(t),
-			Close:           newIndicesCloseFunc(t),
-			Create:          newIndicesCreateFunc(t),
-			Delete:          newIndicesDeleteFunc(t),
-			DeleteAlias:     newIndicesDeleteAliasFunc(t),
-			DeleteTemplate:  newIndicesDeleteTemplateFunc(t),
-			Exists:          newIndicesExistsFunc(t),
-			ExistsAlias:     newIndicesExistsAliasFunc(t),
-			ExistsTemplate:  newIndicesExistsTemplateFunc(t),
-			ExistsType:      newIndicesExistsTypeFunc(t),
-			Flush:           newIndicesFlushFunc(t),
-			FlushSynced:     newIndicesFlushSyncedFunc(t),
-			Forcemerge:      newIndicesForcemergeFunc(t),
-			Get:             newIndicesGetFunc(t),
-			GetAlias:        newIndicesGetAliasFunc(t),
-			GetFieldMapping: newIndicesGetFieldMappingFunc(t),
-			GetMapping:      newIndicesGetMappingFunc(t),
-			GetSettings:     newIndicesGetSettingsFunc(t),
-			GetTemplate:     newIndicesGetTemplateFunc(t),
-			GetUpgrade:      newIndicesGetUpgradeFunc(t),
-			Open:            newIndicesOpenFunc(t),
-			PutAlias:        newIndicesPutAliasFunc(t),
-			PutMapping:      newIndicesPutMappingFunc(t),
-			PutSettings:     newIndicesPutSettingsFunc(t),
-			PutTemplate:     newIndicesPutTemplateFunc(t),
-			Recovery:        newIndicesRecoveryFunc(t),
-			Refresh:         newIndicesRefreshFunc(t),
-			Rollover:        newIndicesRolloverFunc(t),
-			Segments:        newIndicesSegmentsFunc(t),
-			ShardStores:     newIndicesShardStoresFunc(t),
-			Shrink:          newIndicesShrinkFunc(t),
-			Stats:           newIndicesStatsFunc(t),
-			UpdateAliases:   newIndicesUpdateAliasesFunc(t),
-			Upgrade:         newIndicesUpgradeFunc(t),
-			ValidateQuery:   newIndicesValidateQueryFunc(t),
+			Analyze:            newIndicesAnalyzeFunc(t),
+			ClearCache:         newIndicesClearCacheFunc(t),
+			Close:              newIndicesCloseFunc(t),
+			Create:             newIndicesCreateFunc(t),
+			DeleteAlias:        newIndicesDeleteAliasFunc(t),
+			Delete:             newIndicesDeleteFunc(t),
+			DeleteTemplate:     newIndicesDeleteTemplateFunc(t),
+			ExistsAlias:        newIndicesExistsAliasFunc(t),
+			ExistsDocumentType: newIndicesExistsDocumentTypeFunc(t),
+			Exists:             newIndicesExistsFunc(t),
+			ExistsTemplate:     newIndicesExistsTemplateFunc(t),
+			Flush:              newIndicesFlushFunc(t),
+			FlushSynced:        newIndicesFlushSyncedFunc(t),
+			Forcemerge:         newIndicesForcemergeFunc(t),
+			GetAlias:           newIndicesGetAliasFunc(t),
+			GetFieldMapping:    newIndicesGetFieldMappingFunc(t),
+			GetMapping:         newIndicesGetMappingFunc(t),
+			Get:                newIndicesGetFunc(t),
+			GetSettings:        newIndicesGetSettingsFunc(t),
+			GetTemplate:        newIndicesGetTemplateFunc(t),
+			GetUpgrade:         newIndicesGetUpgradeFunc(t),
+			Open:               newIndicesOpenFunc(t),
+			PutAlias:           newIndicesPutAliasFunc(t),
+			PutMapping:         newIndicesPutMappingFunc(t),
+			PutSettings:        newIndicesPutSettingsFunc(t),
+			PutTemplate:        newIndicesPutTemplateFunc(t),
+			Recovery:           newIndicesRecoveryFunc(t),
+			Refresh:            newIndicesRefreshFunc(t),
+			Rollover:           newIndicesRolloverFunc(t),
+			Segments:           newIndicesSegmentsFunc(t),
+			ShardStores:        newIndicesShardStoresFunc(t),
+			Shrink:             newIndicesShrinkFunc(t),
+			Split:              newIndicesSplitFunc(t),
+			Stats:              newIndicesStatsFunc(t),
+			UpdateAliases:      newIndicesUpdateAliasesFunc(t),
+			Upgrade:            newIndicesUpgradeFunc(t),
+			ValidateQuery:      newIndicesValidateQueryFunc(t),
 		},
 		Ingest: &Ingest{
 			DeletePipeline: newIngestDeletePipelineFunc(t),
@@ -296,18 +308,20 @@ func New(t Transport) *API {
 			Simulate:       newIngestSimulateFunc(t),
 		},
 		Nodes: &Nodes{
-			HotThreads: newNodesHotThreadsFunc(t),
-			Info:       newNodesInfoFunc(t),
-			Stats:      newNodesStatsFunc(t),
+			HotThreads:           newNodesHotThreadsFunc(t),
+			Info:                 newNodesInfoFunc(t),
+			ReloadSecureSettings: newNodesReloadSecureSettingsFunc(t),
+			Stats:                newNodesStatsFunc(t),
+			Usage:                newNodesUsageFunc(t),
 		},
 		Remote: &Remote{},
 		Snapshot: &Snapshot{
-			Create:           newSnapshotCreateFunc(t),
 			CreateRepository: newSnapshotCreateRepositoryFunc(t),
-			Delete:           newSnapshotDeleteFunc(t),
+			Create:           newSnapshotCreateFunc(t),
 			DeleteRepository: newSnapshotDeleteRepositoryFunc(t),
-			Get:              newSnapshotGetFunc(t),
+			Delete:           newSnapshotDeleteFunc(t),
 			GetRepository:    newSnapshotGetRepositoryFunc(t),
+			Get:              newSnapshotGetFunc(t),
 			Restore:          newSnapshotRestoreFunc(t),
 			Status:           newSnapshotStatusFunc(t),
 			VerifyRepository: newSnapshotVerifyRepositoryFunc(t),
