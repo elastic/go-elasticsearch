256a620be07dab753f4d69a05f1b313ed335c37e
Karel Minarik <karel.minarik@gmail.com>
2019-07-14 16:28:58 +0200

    Transport: Clean up the Perform() method
    Extract the logging code into a separate method, use duplicateBody() helper method, annotate the code.
    
    (cherry picked from commit 7afe9bd9f47c4fc4efe00b7e7b948f66fbc33063)

--- estransport/estransport.go
+++ estransport/estransport.go
@@ -3,6 +3,7 @@ package estransport
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -82,50 +83,42 @@ func New(cfg Config) *Client {
 // Perform executes the request and returns a response or error.
 //
 func (c *Client) Perform(req *http.Request) (*http.Response, error) {
+	var (
+		dupReqBody io.Reader
+	)
+
+	// Get URL from the Selector
+	//
 	u, err := c.getURL()
 	if err != nil {
 		// TODO(karmi): Log error
 		return nil, fmt.Errorf("cannot get URL: %s", err)
 	}
 
+	// Update request
+	//
 	c.setURL(u, req)
 	c.setUserAgent(req)
+	c.setAuthorization(u, req)
 
-	if _, ok := req.Header["Authorization"]; !ok {
-		c.setAuthorization(u, req)
-	}
-
-	var dupReqBody *bytes.Buffer
+	// Duplicate request body for logger
+	//
 	if c.logger != nil && c.logger.RequestBodyEnabled() {
 		if req.Body != nil && req.Body != http.NoBody {
-			dupReqBody = bytes.NewBuffer(make([]byte, 0, int(req.ContentLength)))
-			dupReqBody.ReadFrom(req.Body)
-			req.Body = ioutil.NopCloser(bytes.NewBuffer(dupReqBody.Bytes()))
+			dupReqBody, req.Body, _ = duplicateBody(req.Body)
 		}
 	}
 
+	// Set up time measures and execute the request
+	//
 	start := time.Now().UTC()
 	res, err := c.transport.RoundTrip(req)
 	dur := time.Since(start)
 
+	// Log request and response
+	//
 	if c.logger != nil {
-		var dupRes http.Response
-		if res != nil {
-			dupRes = *res
-		}
-		if c.logger.RequestBodyEnabled() {
-			if req.Body != nil && req.Body != http.NoBody {
-				req.Body = ioutil.NopCloser(dupReqBody)
-			}
-		}
-		if c.logger.ResponseBodyEnabled() {
-			if res != nil && res.Body != nil && res.Body != http.NoBody {
-				b1, b2, _ := duplicateBody(res.Body)
-				dupRes.Body = b1
-				res.Body = b2
-			}
-		}
-		c.logger.LogRoundTrip(req, &dupRes, err, start, dur) // errcheck exclude
+		c.logRoundTrip(req, res, dupReqBody, err, start, dur)
 	}
 
 	// TODO(karmi): Wrap error
@@ -158,24 +151,26 @@ func (c *Client) setURL(u *url.URL, req *http.Request) *http.Request {
 }
 
 func (c *Client) setAuthorization(u *url.URL, req *http.Request) *http.Request {
-	if u.User != nil {
-		password, _ := u.User.Password()
-		req.SetBasicAuth(u.User.Username(), password)
-		return req
-	}
+	if _, ok := req.Header["Authorization"]; !ok {
+		if u.User != nil {
+			password, _ := u.User.Password()
+			req.SetBasicAuth(u.User.Username(), password)
+			return req
+		}
 
-	if c.apikey != "" {
-		var b bytes.Buffer
-		b.Grow(len("APIKey ") + len(c.apikey))
-		b.WriteString("APIKey ")
-		b.WriteString(c.apikey)
-		req.Header.Set("Authorization", b.String())
-		return req
-	}
+		if c.apikey != "" {
+			var b bytes.Buffer
+			b.Grow(len("APIKey ") + len(c.apikey))
+			b.WriteString("APIKey ")
+			b.WriteString(c.apikey)
+			req.Header.Set("Authorization", b.String())
+			return req
+		}
 
-	if c.username != "" && c.password != "" {
-		req.SetBasicAuth(c.username, c.password)
-		return req
+		if c.username != "" && c.password != "" {
+			req.SetBasicAuth(c.username, c.password)
+			return req
+		}
 	}
 
 	return req
@@ -184,6 +179,33 @@ func (c *Client) setAuthorization(u *url.URL, req *http.Request) *http.Request {
 func (c *Client) setUserAgent(req *http.Request) *http.Request {
 	req.Header.Set("User-Agent", userAgent)
 	return req
+}
+
+func (c *Client) logRoundTrip(
+	req *http.Request,
+	res *http.Response,
+	reqBody io.Reader,
+	err error,
+	start time.Time,
+	dur time.Duration,
+) {
+	var dupRes http.Response
+	if res != nil {
+		dupRes = *res
+	}
+	if c.logger.RequestBodyEnabled() {
+		if req.Body != nil && req.Body != http.NoBody {
+			req.Body = ioutil.NopCloser(reqBody)
+		}
+	}
+	if c.logger.ResponseBodyEnabled() {
+		if res != nil && res.Body != nil && res.Body != http.NoBody {
+			b1, b2, _ := duplicateBody(res.Body)
+			dupRes.Body = b1
+			res.Body = b2
+		}
+	}
+	c.logger.LogRoundTrip(req, &dupRes, err, start, dur) // errcheck exclude
 }
 
 func initUserAgent() string {
