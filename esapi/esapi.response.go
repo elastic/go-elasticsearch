d33341d0da2b3fe3f24ba9c07ba2d06c623eb7b9
Karel Minarik <karel.minarik@gmail.com>
2019-07-19 15:45:12 +0200

    API: Fix (*esapi.Response).String() to prevent panic when response is nil
    (cherry picked from commit ee4dede054b1bd0f214790cc46171f354a518322)

--- esapi/esapi.response.go
+++ esapi/esapi.response.go
@@ -27,20 +27,32 @@ func (r *Response) String() string {
 		out = new(bytes.Buffer)
 		b1  = bytes.NewBuffer([]byte{})
 		b2  = bytes.NewBuffer([]byte{})
-		tr  = io.TeeReader(r.Body, b1)
+		tr  io.Reader
 	)
 
-	defer r.Body.Close()
+	if r != nil && r.Body != nil {
+		tr = io.TeeReader(r.Body, b1)
+		defer r.Body.Close()
 
-	if _, err := io.Copy(b2, tr); err != nil {
-		out.WriteString(fmt.Sprintf("<error reading response body: %v>", err))
-		return out.String()
+		if _, err := io.Copy(b2, tr); err != nil {
+			out.WriteString(fmt.Sprintf("<error reading response body: %v>", err))
+			return out.String()
+		}
+		defer func() { r.Body = ioutil.NopCloser(b1) }()
 	}
-	defer func() { r.Body = ioutil.NopCloser(b1) }()
 
-	out.WriteString(fmt.Sprintf("[%d %s]", r.StatusCode, http.StatusText(r.StatusCode)))
-	out.WriteRune(' ')
-	out.ReadFrom(b2) // errcheck exclude (*bytes.Buffer).ReadFrom
+	if r != nil {
+		out.WriteString(fmt.Sprintf("[%d %s]", r.StatusCode, http.StatusText(r.StatusCode)))
+		if r.StatusCode > 0 {
+			out.WriteRune(' ')
+		}
+	} else {
+		out.WriteString("[0 <nil>]")
+	}
+
+	if r != nil && r.Body != nil {
+		out.ReadFrom(b2) // errcheck exclude (*bytes.Buffer).ReadFrom
+	}
 
 	return out.String()
 }
@@ -49,9 +61,11 @@ func (r *Response) String() string {
 //
 func (r *Response) Status() string {
 	var b strings.Builder
-	b.WriteString(strconv.Itoa(r.StatusCode))
-	b.WriteString(" ")
-	b.WriteString(http.StatusText(r.StatusCode))
+	if r != nil {
+		b.WriteString(strconv.Itoa(r.StatusCode))
+		b.WriteString(" ")
+		b.WriteString(http.StatusText(r.StatusCode))
+	}
 	return b.String()
 }
 
