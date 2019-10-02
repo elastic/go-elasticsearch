880f6bd895ebe7cbc4b75c1a2571f1b450e244a3
Karel Minarik <karel.minarik@gmail.com>
2019-10-02 11:29:36 +0200

    WIP > Use http.NewRequest instead of constructing the request manually

--- esapi/esapi.request.go
+++ esapi/esapi.request.go
@@ -1,13 +1,9 @@
 package esapi
 
 import (
-	"bytes"
 	"context"
 	"io"
-	"io/ioutil"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
 const (
@@ -27,43 +23,5 @@ type Request interface {
 // newRequest creates an HTTP request.
 //
 func newRequest(method, path string, body io.Reader) (*http.Request, error) {
-	req := http.Request{
-		Method:     method,
-		URL:        &url.URL{Path: path},
-		Proto:      "HTTP/1.1",
-		ProtoMajor: 1,
-		ProtoMinor: 1,
-		Header:     make(http.Header),
-	}
-
-	if body != nil {
-		if body, ok := body.(io.ReadCloser); !ok {
-			req.Body = ioutil.NopCloser(body)
-		} else {
-			req.Body = body
-		}
-
-		switch b := body.(type) {
-		case *bytes.Buffer:
-			req.ContentLength = int64(b.Len())
-			buf := b.Bytes()
-			req.GetBody = func() (io.ReadCloser, error) {
-				r := bytes.NewReader(buf)
-				return ioutil.NopCloser(r), nil
-			}
-		case *bytes.Reader:
-			req.ContentLength = int64(b.Len())
-		case *strings.Reader:
-			req.ContentLength = int64(b.Len())
-		default:
-		}
-	}
-
-	// if body != nil && req.GetBody == nil {
-	// 	req.GetBody = func() (io.ReadCloser, error) {
-	// 		return ioutil.NopCloser(body), nil
-	// 	}
-	// }
-
-	return &req, nil
+	return http.NewRequest(method, path, body)
 }
