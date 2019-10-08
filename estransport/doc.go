7f37b7b9a63588ce18b365d4e07917dbda8e55a0
Karel Minarik <karel.minarik@gmail.com>
2019-10-08 09:53:01 +0200

    Transport: Add support for request retries
    This patch adds the configuration parameters and supporting logic for retrying
    a failed request: a feature common to official Elasticsearch clients.
    
    Executable example:
    
    ```golang
    //+build ignore
    
    package main
    
    import (
    	"context"
    	"fmt"
    	"log"
    	"math"
    	"net"
    	"net/http"
    	"os"
    	"os/signal"
    	"strings"
    	"syscall"
    	"time"
    
    	"github.com/elastic/go-elasticsearch/v8"
    	"github.com/elastic/go-elasticsearch/v8/esapi"
    	"github.com/elastic/go-elasticsearch/v8/estransport"
    
    	"github.com/cenkalti/backoff"
    )
    
    var (
    	_ = fmt.Print
    	_ = context.WithTimeout
    	_ = math.Exp
    	_ = strings.NewReader
    	_ = http.DefaultClient
    )
    
    func main() {
    	log.SetFlags(0)
    
    	ticker := time.NewTicker(time.Second)
    	defer ticker.Stop()
    
    	aborted := make(chan os.Signal)
    	signal.Notify(aborted, os.Interrupt, syscall.SIGTERM)
    
    	retryBackoff := backoff.NewExponentialBackOff()
    	retryBackoff.InitialInterval = time.Second
    	_ = retryBackoff
    
    	cfg := elasticsearch.Config{
    		Addresses: []string{
    			"http://localhost:9200",
    			// "http://localhost:1000",
    			// "http://localhost:2000",
    			// "http://localhost:3000",
    			"http://localhost:4000",
    			"http://localhost:5000",
    			// "http://localhost:6000",
    			// "http://localhost:7000",
    			// "http://localhost:8000",
    			// "http://localhost:9200",
    			// "http://localhost:9000",
    			// "http://localhost:9201",
    			// "http://localhost:9202",
    		},
    
    		Logger: &estransport.ColorLogger{
    			Output:             os.Stdout,
    			EnableRequestBody:  true,
    			EnableResponseBody: true,
    		},
    
    		// RetryOnStatus:        []int{404},
    		// EnableRetryOnTimeout: true,
    		// MaxRetries: 10,
    
    		// RetryBackoff: func(i int) time.Duration {
    		// 	d := time.Duration(i) * time.Second
    		// 	fmt.Printf("Attempt: %d | Sleeping for %s...\n", i, d)
    		// 	return d
    		// },
    
    		// RetryBackoff: func(i int) time.Duration {
    		// 	d := time.Duration(math.Exp2(float64(i))) * time.Second
    		// 	fmt.Printf("Attempt: %d | Sleeping for %s...\n", i, d)
    		// 	return d
    		// },
    
    		RetryBackoff: func(i int) time.Duration {
    			if i == 1 {
    				retryBackoff.Reset()
    			}
    			d := retryBackoff.NextBackOff()
    			fmt.Printf("Attempt: %d | Sleeping for %s...\n", i, d)
    			return d
    		},
    
    		// Transport: &http.Transport{
    		// 	ResponseHeaderTimeout: time.Millisecond,
    		// },
    	}
    
    	es, err := elasticsearch.NewClient(cfg)
    	if err != nil {
    		log.Fatalf("Error creating the client: %s", err)
    	}
    	log.Println("Client ready with URLs:", es.Transport.(*estransport.Client).URLs())
    
    	go func() {
    		<-aborted
    		log.Println("\nDone!")
    		os.Exit(0)
    	}()
    
    	for {
    		select {
    		case <-ticker.C:
    			var (
    				res *esapi.Response
    				err error
    			)
    			// res, err = es.Info()
    
    			// ctx, cancel := context.WithTimeout(context.Background(), time.Nanosecond)
    			// defer cancel()
    			// res, err = es.Info(es.Info.WithContext(ctx))
    
    			// es.Search(es.Search.WithTimeout(time.Nanosecond))
    
    			// res, err = es.Get("test", "MISSING")
    
    			res, err = es.Index("test", strings.NewReader(`{"foo":"bar"}`))
    
    			if err != nil {
    				if e, ok := err.(net.Error); ok {
    					log.Fatalf("Error getting response: [%T]: %s (timeout:%v)", e, e, e.Timeout())
    				} else {
    					log.Fatalf("Error getting response: [%T]: %s", err, err)
    				}
    			}
    			if res.IsError() {
    				log.Fatalf("Error response: %s", res.Status())
    			}
    		}
    	}
    }
    
    ```
    
    Closes #67

--- estransport/doc.go
+++ estransport/doc.go
@@ -4,11 +4,22 @@ Package estransport provides the transport layer for the Elasticsearch client.
 It is automatically included in the client provided by the github.com/elastic/go-elasticsearch package
 and is not intended for direct use: to configure the client, use the elasticsearch.Config struct.
 
-The default HTTP transport of the client is http.Transport.
+The default HTTP transport of the client is http.Transport; use the Transport option to customize it;
+see the _examples/customization.go file in this repository for information.
 
 The package defines the "Selector" interface for getting a URL from the list. At the moment,
 the implementation is rather minimal: the client takes a slice of url.URL pointers,
 and round-robins across them when performing the request.
+
+The package will automatically retry requests on network-related errors, and on specific
+response status codes (by default 502, 503, 504). Use the RetryOnStatus option to customize the list.
+The transport will not retry a timeout network error, unless enabled by setting EnableRetryOnTimeout to true.
+
+Use the MaxRetries option to configure the number of retries, and set DisableRetry to true
+to disable the retry behaviour altogether.
+
+By default, the retry will be performed without any delay; to configure a backoff interval,
+implement the RetryBackoff option function; see an example in the package unit tests for information.
 
 The package defines the "Logger" interface for logging information about request and response.
 It comes with several bundled loggers for logging in text and JSON.
