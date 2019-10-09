elastic/go-elasticsearch

commit 7f37b7b9a63588ce18b365d4e07917dbda8e55a0

Transport: Add support for request retries
This patch adds the configuration parameters and supporting logic for retrying
a failed request: a feature common to official Elasticsearch clients.

Executable example:

```golang
//+build ignore

package main

import (
	"context"
	"fmt"
	"log"
	"math"
	"net"
	"net/http"
	"os"
	"os/signal"
	"strings"
	"syscall"
	"time"

	"github.com/elastic/go-elasticsearch/v8"
	"github.com/elastic/go-elasticsearch/v8/esapi"
	"github.com/elastic/go-elasticsearch/v8/estransport"

	"github.com/cenkalti/backoff"
)

var (
	_ = fmt.Print
	_ = context.WithTimeout
	_ = math.Exp
	_ = strings.NewReader
	_ = http.DefaultClient
)

func main() {
	log.SetFlags(0)

	ticker := time.NewTicker(time.Second)
	defer ticker.Stop()

	aborted := make(chan os.Signal)
	signal.Notify(aborted, os.Interrupt, syscall.SIGTERM)

	retryBackoff := backoff.NewExponentialBackOff()
	retryBackoff.InitialInterval = time.Second
	_ = retryBackoff

	cfg := elasticsearch.Config{
		Addresses: []string{
			"http://localhost:9200",
			// "http://localhost:1000",
			// "http://localhost:2000",
			// "http://localhost:3000",
			"http://localhost:4000",
			"http://localhost:5000",
			// "http://localhost:6000",
			// "http://localhost:7000",
			// "http://localhost:8000",
			// "http://localhost:9200",
			// "http://localhost:9000",
			// "http://localhost:9201",
			// "http://localhost:9202",
		},

		Logger: &estransport.ColorLogger{
			Output:             os.Stdout,
			EnableRequestBody:  true,
			EnableResponseBody: true,
		},

		// RetryOnStatus:        []int{404},
		// EnableRetryOnTimeout: true,
		// MaxRetries: 10,

		// RetryBackoff: func(i int) time.Duration {
		// 	d := time.Duration(i) * time.Second
		// 	fmt.Printf("Attempt: %d | Sleeping for %s...\n", i, d)
		// 	return d
		// },

		// RetryBackoff: func(i int) time.Duration {
		// 	d := time.Duration(math.Exp2(float64(i))) * time.Second
		// 	fmt.Printf("Attempt: %d | Sleeping for %s...\n", i, d)
		// 	return d
		// },

estransport/doc.go
@@ -4,11 +4,22 @@ Package estransport provides the transport layer for the Elasticsearch client.
 It is automatically included in the client provided by the github.com/elastic/go-elasticsearch package
 and is not intended for direct use: to configure the client, use the elasticsearch.Config struct.
 
-The default HTTP transport of the client is http.Transport.
+The default HTTP transport of the client is http.Transport; use the Transport option to customize it;
+see the _examples/customization.go file in this repository for information.
 
 The package defines the "Selector" interface for getting a URL from the list. At the moment,
 the implementation is rather minimal: the client takes a slice of url.URL pointers,
 and round-robins across them when performing the request.
+
+The package will automatically retry requests on network-related errors, and on specific
+response status codes (by default 502, 503, 504). Use the RetryOnStatus option to customize the list.
+The transport will not retry a timeout network error, unless enabled by setting EnableRetryOnTimeout to true.
+
+Use the MaxRetries option to configure the number of retries, and set DisableRetry to true
+to disable the retry behaviour altogether.
+
+By default, the retry will be performed without any delay; to configure a backoff interval,
+implement the RetryBackoff option function; see an example in the package unit tests for information.
 
 The package defines the "Logger" interface for logging information about request and response.
 It comes with several bundled loggers for logging in text and JSON.

estransport/estransport.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -22,6 +23,9 @@ const Version = version.Client
 var (
 	userAgent   string
 	reGoVersion = regexp.MustCompile(`go(\d+\.\d+\..+)`)
+
+	defaultMaxRetries    = 3
+	defaultRetryOnStatus = [...]int{502, 503, 504}
 )
 
 func init() {
@@ -42,6 +46,12 @@ type Config struct {
 	Password string
 	APIKey   string
 
+	RetryOnStatus        []int
+	DisableRetry         bool
+	EnableRetryOnTimeout bool
+	MaxRetries           int
+	RetryBackoff         func(attempt int) time.Duration
+
 	Transport http.RoundTripper
 	Logger    Logger
 }
@@ -53,6 +63,12 @@ type Client struct {
 	username string
 	password string
 	apikey   string
+
+	retryOnStatus        []int
+	disableRetry         bool
+	enableRetryOnTimeout bool
+	maxRetries           int
+	retryBackoff         func(attempt int) time.Duration
 
 	transport http.RoundTripper
 	selector  Selector
@@ -68,11 +84,25 @@ func New(cfg Config) *Client {
 		cfg.Transport = http.DefaultTransport
 	}
 
+	if len(cfg.RetryOnStatus) == 0 {
+		cfg.RetryOnStatus = defaultRetryOnStatus[:]
+	}
+
+	if cfg.MaxRetries == 0 {
+		cfg.MaxRetries = defaultMaxRetries
+	}
+
 	return &Client{
 		urls:     cfg.URLs,
 		username: cfg.Username,
 		password: cfg.Password,
 		apikey:   cfg.APIKey,
+
+		retryOnStatus:        cfg.RetryOnStatus,
+		disableRetry:         cfg.DisableRetry,
+		enableRetryOnTimeout: cfg.EnableRetryOnTimeout,
+		maxRetries:           cfg.MaxRetries,
+		retryBackoff:         cfg.RetryBackoff,
 
 		transport: cfg.Transport,
 		selector:  NewRoundRobinSelector(cfg.URLs...),
@@ -84,41 +114,86 @@ func New(cfg Config) *Client {
 //
 func (c *Client) Perform(req *http.Request) (*http.Response, error) {
 	var (
-		dupReqBody io.Reader
-	)
+		res *http.Response
+		err error
 
-	// Get URL from the Selector
-	//
-	u, err := c.getURL()
-	if err != nil {
-		// TODO(karmi): Log error
-		return nil, fmt.Errorf("cannot get URL: %s", err)
-	}
+		dupReqBodyForLog io.ReadCloser
+	)
 
 	// Update request
 	//
-	c.setURL(u, req)
-	c.setUserAgent(req)
-	c.setAuthorization(u, req)
+	c.setReqUserAgent(req)
 
-	// Duplicate request body for logger
-	//
-	if c.logger != nil && c.logger.RequestBodyEnabled() {
-		if req.Body != nil && req.Body != http.NoBody {
-			dupReqBody, req.Body, _ = duplicateBody(req.Body)
+	for i := 1; i <= c.maxRetries; i++ {
+		var (
+			nodeURL     *url.URL
+			shouldRetry bool
+		)
+
+		// Get URL from the Selector
+		//
+		nodeURL, err = c.getURL()
+		if err != nil {
+			// TODO(karmi): Log error
+			return nil, fmt.Errorf("cannot get URL: %s", err)
 		}
-	}
 
-	// Set up time measures and execute the request
-	//
-	start := time.Now().UTC()
-	res, err := c.transport.RoundTrip(req)
-	dur := time.Since(start)
+		// Update request
+		//
+		c.setReqURL(nodeURL, req)
+		c.setReqAuth(nodeURL, req)
 
-	// Log request and response
-	//
-	if c.logger != nil {
-		c.logRoundTrip(req, res, dupReqBody, err, start, dur)
+		// Duplicate request body for logger
+		//
+		if c.logger != nil && c.logger.RequestBodyEnabled() {
+			if req.Body != nil && req.Body != http.NoBody {
+				dupReqBodyForLog, req.Body, _ = duplicateBody(req.Body)
+			}
+		}
+
+		// Set up time measures and execute the request
+		//
+		start := time.Now().UTC()
+		res, err = c.transport.RoundTrip(req)
+		dur := time.Since(start)
+
+		// Log request and response
+		//
+		if c.logger != nil {
+			c.logRoundTrip(req, res, dupReqBodyForLog, err, start, dur)
+		}
+
+		// Retry only on network errors, but don't retry on timeout errors, unless configured
+		//
+		if err != nil {
+			if err, ok := err.(net.Error); ok {
+				if (!err.Timeout() || c.enableRetryOnTimeout) && !c.disableRetry {
+					shouldRetry = true
+				}
+			}
+		}
+
+		// Retry on configured response statuses
+		//
+		if res != nil && !c.disableRetry {
+			for _, code := range c.retryOnStatus {
+				if res.StatusCode == code {
+					shouldRetry = true
+				}
+			}
+		}
+
+		// Break if retry should not be performed
+		//
+		if !shouldRetry {
+			break
+		}
+
+		// Delay the retry if a backoff function is configured
+		//
+		if c.retryBackoff != nil {
+			time.Sleep(c.retryBackoff(i))
+		}
 	}
 
 	// TODO(karmi): Wrap error
@@ -135,7 +210,7 @@ func (c *Client) getURL() (*url.URL, error) {
 	return c.selector.Select()
 }
 
-func (c *Client) setURL(u *url.URL, req *http.Request) *http.Request {
+func (c *Client) setReqURL(u *url.URL, req *http.Request) *http.Request {
 	req.URL.Scheme = u.Scheme
 	req.URL.Host = u.Host
 
@@ -150,7 +225,7 @@ func (c *Client) setURL(u *url.URL, req *http.Request) *http.Request {
 	return req
 }
 
-func (c *Client) setAuthorization(u *url.URL, req *http.Request) *http.Request {
+func (c *Client) setReqAuth(u *url.URL, req *http.Request) *http.Request {
 	if _, ok := req.Header["Authorization"]; !ok {
 		if u.User != nil {
 			password, _ := u.User.Password()
@@ -176,7 +251,7 @@ func (c *Client) setAuthorization(u *url.URL, req *http.Request) *http.Request {
 	return req
 }
 
-func (c *Client) setUserAgent(req *http.Request) *http.Request {
+func (c *Client) setReqUserAgent(req *http.Request) *http.Request {
 	req.Header.Set("User-Agent", userAgent)
 	return req
 }

estransport/estransport_internal_test.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"reflect"
 	"strings"
 	"testing"
+	"time"
 )
 
 var (
@@ -21,6 +23,11 @@ type mockTransp struct {
 func (t *mockTransp) RoundTrip(req *http.Request) (*http.Response, error) {
 	return t.RoundTripFunc(req)
 }
+
+type mockNetError struct{ error }
+
+func (e *mockNetError) Timeout() bool   { return false }
+func (e *mockNetError) Temporary() bool { return false }
 
 func TestTransport(t *testing.T) {
 	t.Run("Interface", func(t *testing.T) {
@@ -57,6 +64,53 @@ func TestTransport(t *testing.T) {
 	})
 }
 
+func TestTransportConfig(t *testing.T) {
+	t.Run("Defaults", func(t *testing.T) {
+		tp := New(Config{})
+
+		if !reflect.DeepEqual(tp.retryOnStatus, []int{502, 503, 504}) {
+			t.Errorf("Unexpected retryOnStatus: %v", tp.retryOnStatus)
+		}
+
+		if tp.disableRetry {
+			t.Errorf("Unexpected disableRetry: %v", tp.disableRetry)
+		}
+
+		if tp.enableRetryOnTimeout {
+			t.Errorf("Unexpected enableRetryOnTimeout: %v", tp.enableRetryOnTimeout)
+		}
+
+		if tp.maxRetries != 3 {
+			t.Errorf("Unexpected maxRetries: %v", tp.maxRetries)
+		}
+	})
+
+	t.Run("Custom", func(t *testing.T) {
+		tp := New(Config{
+			RetryOnStatus:        []int{404, 408},
+			DisableRetry:         true,
+			EnableRetryOnTimeout: true,
+			MaxRetries:           5,
+		})
+
+		if !reflect.DeepEqual(tp.retryOnStatus, []int{404, 408}) {
+			t.Errorf("Unexpected retryOnStatus: %v", tp.retryOnStatus)
+		}
+
+		if !tp.disableRetry {
+			t.Errorf("Unexpected disableRetry: %v", tp.disableRetry)
+		}
+
+		if !tp.enableRetryOnTimeout {
+			t.Errorf("Unexpected enableRetryOnTimeout: %v", tp.enableRetryOnTimeout)
+		}
+
+		if tp.maxRetries != 5 {
+			t.Errorf("Unexpected maxRetries: %v", tp.maxRetries)
+		}
+	})
+}
+
 func TestTransportPerform(t *testing.T) {
 	t.Run("Executes", func(t *testing.T) {
 		u, _ := url.Parse("https://foo.com/bar")
@@ -83,7 +137,7 @@ func TestTransportPerform(t *testing.T) {
 		tp := New(Config{URLs: []*url.URL{u}})
 
 		req, _ := http.NewRequest("GET", "/abc", nil)
-		tp.setURL(u, req)
+		tp.setReqURL(u, req)
 
 		expected := "https://foo.com/bar/abc"
 
@@ -97,7 +151,7 @@ func TestTransportPerform(t *testing.T) {
 		tp := New(Config{URLs: []*url.URL{u}})
 
 		req, _ := http.NewRequest("GET", "/", nil)
-		tp.setAuthorization(u, req)
+		tp.setReqAuth(u, req)
 
 		username, password, ok := req.BasicAuth()
 		if !ok {
@@ -114,7 +168,7 @@ func TestTransportPerform(t *testing.T) {
 		tp := New(Config{URLs: []*url.URL{u}, Username: "foo", Password: "bar"})
 
 		req, _ := http.NewRequest("GET", "/", nil)
-		tp.setAuthorization(u, req)
+		tp.setReqAuth(u, req)
 
 		username, password, ok := req.BasicAuth()
 		if !ok {
@@ -131,7 +185,7 @@ func TestTransportPerform(t *testing.T) {
 		tp := New(Config{URLs: []*url.URL{u}, APIKey: "Zm9vYmFy"}) // foobar
 
 		req, _ := http.NewRequest("GET", "/", nil)
-		tp.setAuthorization(u, req)
+		tp.setReqAuth(u, req)
 
 		value := req.Header.Get("Authorization")
 		if value == "" {
@@ -148,7 +202,7 @@ func TestTransportPerform(t *testing.T) {
 		tp := New(Config{URLs: []*url.URL{u}})
 
 		req, _ := http.NewRequest("GET", "/abc", nil)
-		tp.setUserAgent(req)
+		tp.setReqUserAgent(req)
 
 		if !strings.HasPrefix(req.UserAgent(), "go-elasticsearch") {
 			t.Errorf("Unexpected user agent: %s", req.UserAgent())
@@ -167,6 +221,233 @@ func TestTransportPerform(t *testing.T) {
 		res, err := tp.Perform(req)
 		if err.Error() != "cannot get URL: No URL available" {
 			t.Fatalf("Expected error `cannot get URL`: but got error %v, response %v", err, res)
+		}
+	})
+}
+
+func TestTransportPerformRetries(t *testing.T) {
+	t.Run("Retry request on network error and return the response", func(t *testing.T) {
+		var (
+			i       int
+			numReqs = 2
+		)
+
+		u, _ := url.Parse("http://foo.bar")
+		tp := New(Config{
+			URLs: []*url.URL{u, u, u},
+			Transport: &mockTransp{
+				RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+					i++
+					fmt.Printf("Request #%d", i)
+					if i == numReqs {
+						fmt.Print(": OK\n")
+						return &http.Response{Status: "OK"}, nil
+					}
+					fmt.Print(": ERR\n")
+					return nil, &mockNetError{error: fmt.Errorf("Mock network error (%d)", i)}
+				},
+			}})
+
+		req, _ := http.NewRequest("GET", "/abc", nil)
+
+		res, err := tp.Perform(req)
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		if res.Status != "OK" {
+			t.Errorf("Unexpected response: %+v", res)
+		}
+
+		if i != numReqs {
+			t.Errorf("Unexpected number of requests, want=%d, got=%d", numReqs, i)
+		}
+	})
+
+	t.Run("Retry request on 5xx response and return new response", func(t *testing.T) {
+		var (
+			i       int
+			numReqs = 2
+		)
+
+		u, _ := url.Parse("http://foo.bar")
+		tp := New(Config{
+			URLs: []*url.URL{u, u, u},
+			Transport: &mockTransp{
+				RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+					i++
+					fmt.Printf("Request #%d", i)
+					if i == numReqs {
+						fmt.Print(": 200\n")
+						return &http.Response{StatusCode: 200}, nil
+					}
+					fmt.Print(": 502\n")
+					return &http.Response{StatusCode: 502}, nil
+				},
+			}})
+
+		req, _ := http.NewRequest("GET", "/abc", nil)
+
+		res, err := tp.Perform(req)
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		if res.StatusCode != 200 {
+			t.Errorf("Unexpected response: %+v", res)
+		}
+
+		if i != numReqs {
+			t.Errorf("Unexpected number of requests, want=%d, got=%d", numReqs, i)
+		}
+	})
+
+	t.Run("Retry request and return error when max retries exhausted", func(t *testing.T) {
+		var (
+			i       int
+			numReqs = 3
+		)
+
+		u, _ := url.Parse("http://foo.bar")
+		tp := New(Config{
+			URLs: []*url.URL{u, u, u},
+			Transport: &mockTransp{
+				RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+					i++
+					fmt.Printf("Request #%d", i)
+					fmt.Print(": ERR\n")
+					return nil, &mockNetError{error: fmt.Errorf("Mock network error (%d)", i)}
+				},
+			}})
+
+		req, _ := http.NewRequest("GET", "/abc", nil)
+
+		res, err := tp.Perform(req)
+
+		if err == nil {
+			t.Fatalf("Expected error, got: %v", err)
+		}
+
+		if res != nil {
+			t.Errorf("Unexpected response: %+v", res)
+		}
+
+		if i != numReqs {
+			t.Errorf("Unexpected number of requests, want=%d, got=%d", numReqs, i)
+		}
+	})
+
+	t.Run("Don't retry request on regular error", func(t *testing.T) {
+		var i int
+
+		u, _ := url.Parse("http://foo.bar")
+		tp := New(Config{
+			URLs: []*url.URL{u, u, u},
+			Transport: &mockTransp{
+				RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+					i++
+					fmt.Printf("Request #%d", i)
+					fmt.Print(": ERR\n")
+					return nil, fmt.Errorf("Mock regular error (%d)", i)
+				},
+			}})
+
+		req, _ := http.NewRequest("GET", "/abc", nil)
+
+		res, err := tp.Perform(req)
+
+		if err == nil {
+			t.Fatalf("Expected error, got: %v", err)
+		}
+
+		if res != nil {
+			t.Errorf("Unexpected response: %+v", res)
+		}
+
+		if i != 1 {
+			t.Errorf("Unexpected number of requests, want=%d, got=%d", 1, i)
+		}
+	})
+
+	t.Run("Don't retry request when retries are disabled", func(t *testing.T) {
+		var i int
+
+		u, _ := url.Parse("http://foo.bar")
+		tp := New(Config{
+			URLs: []*url.URL{u, u, u},
+			Transport: &mockTransp{
+				RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+					i++
+					fmt.Printf("Request #%d", i)
+					fmt.Print(": ERR\n")
+					return nil, &mockNetError{error: fmt.Errorf("Mock network error (%d)", i)}
+				},
+			},
+			DisableRetry: true,
+		})
+
+		req, _ := http.NewRequest("GET", "/abc", nil)
+		tp.Perform(req)
+
+		if i != 1 {
+			t.Errorf("Unexpected number of requests, want=%d, got=%d", 1, i)
+		}
+	})
+
+	t.Run("Delay the retry with a backoff function", func(t *testing.T) {
+		var (
+			i                int
+			numReqs          = 3
+			start            = time.Now()
+			expectedDuration = time.Duration(numReqs*100) * time.Millisecond
+		)
+
+		u, _ := url.Parse("http://foo.bar")
+		tp := New(Config{
+			URLs: []*url.URL{u, u, u},
+			Transport: &mockTransp{
+				RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+					i++
+					fmt.Printf("Request #%d", i)
+					if i == numReqs {
+						fmt.Print(": OK\n")
+						return &http.Response{Status: "OK"}, nil
+					}
+					fmt.Print(": ERR\n")
+					return nil, &mockNetError{error: fmt.Errorf("Mock network error (%d)", i)}
+				},
+			},
+
+			// A simple incremental backoff function
+			//
+			RetryBackoff: func(i int) time.Duration {
+				d := time.Duration(i) * 100 * time.Millisecond
+				fmt.Printf("Attempt: %d | Sleeping for %s...\n", i, d)
+				return d
+			},
+		})
+
+		req, _ := http.NewRequest("GET", "/abc", nil)
+
+		res, err := tp.Perform(req)
+		end := time.Since(start)
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		if res.Status != "OK" {
+			t.Errorf("Unexpected response: %+v", res)
+		}
+
+		if i != numReqs {
+			t.Errorf("Unexpected number of requests, want=%d, got=%d", numReqs, i)
+		}
+
+		if end < expectedDuration {
+			t.Errorf("Unexpected duration, want=>%s, got=%s", expectedDuration, end)
 		}
 	})
 }

doc.go
@@ -42,6 +42,8 @@ Call the Elasticsearch APIs by invoking the corresponding methods on the client:
 
 		log.Println(res)
 
-See the github.com/elastic/go-elasticsearch/esapi package for more information and examples.
+See the github.com/elastic/go-elasticsearch/esapi package for more information about using the API.
+
+See the github.com/elastic/go-elasticsearch/estransport package for more information about configuring the transport.
 */
 package elasticsearch

esapi/doc.go
@@ -85,10 +85,11 @@ about the API endpoints and parameters.
 The Go API is generated from the Elasticsearch JSON specification at
 https://github.com/elastic/elasticsearch/tree/master/rest-api-spec/src/main/resources/rest-api-spec/api
 by the internal package available at
-https://github.com/elastic/go-elasticsearch/tree/master/internal/cmd/generate/commands.
+https://github.com/elastic/go-elasticsearch/tree/master/internal/cmd/generate/commands/gensource.
 
 The API is tested by integration tests common to all Elasticsearch official clients, generated from the
-source at https://github.com/elastic/elasticsearch/tree/master/rest-api-spec/src/main/resources/rest-api-spec/test. The generator is provided by the internal package internal/cmd/generate.
+source at https://github.com/elastic/elasticsearch/tree/master/rest-api-spec/src/main/resources/rest-api-spec/test.
+The generator is provided by the internal package available at internal/cmd/generate/commands/gentests.
 
 */
 package esapi

elasticsearch.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"github.com/elastic/go-elasticsearch/v8/estransport"
@@ -31,6 +32,13 @@ type Config struct {
 
 	CloudID string // Endpoint for the Elastic Service (https://elastic.co/cloud).
 	APIKey  string // Base64-encoded token for authorization; if set, overrides username and password.
+
+	RetryOnStatus        []int // List of status codes for retry. Default: 502, 503, 504.
+	DisableRetry         bool  // Default: false.
+	EnableRetryOnTimeout bool  // Default: false.
+	MaxRetries           int   // Default: 3.
+
+	RetryBackoff func(attempt int) time.Duration // Optional backoff duration. Default: nil.
 
 	Transport http.RoundTripper  // The HTTP transport object.
 	Logger    estransport.Logger // The logger object.
@@ -111,6 +119,12 @@ func NewClient(cfg Config) (*Client, error) {
 		Username: cfg.Username,
 		Password: cfg.Password,
 		APIKey:   cfg.APIKey,
+
+		RetryOnStatus:        cfg.RetryOnStatus,
+		DisableRetry:         cfg.DisableRetry,
+		EnableRetryOnTimeout: cfg.EnableRetryOnTimeout,
+		MaxRetries:           cfg.MaxRetries,
+		RetryBackoff:         cfg.RetryBackoff,
 
 		Transport: cfg.Transport,
 		Logger:    cfg.Logger,

esapi/esapi.request.go
@@ -1,13 +1,9 @@
 package esapi
 
 import (
-	"bytes"
 	"context"
 	"io"
-	"io/ioutil"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
 const (
@@ -27,30 +23,5 @@ type Request interface {
 // newRequest creates an HTTP request.
 //
 func newRequest(method, path string, body io.Reader) (*http.Request, error) {
-	r := http.Request{
-		Method:     method,
-		URL:        &url.URL{Path: path},
-		Proto:      "HTTP/1.1",
-		ProtoMajor: 1,
-		ProtoMinor: 1,
-		Header:     make(http.Header),
-	}
-
-	if body != nil {
-		switch b := body.(type) {
-		case *bytes.Buffer:
-			r.Body = ioutil.NopCloser(body)
-			r.ContentLength = int64(b.Len())
-		case *bytes.Reader:
-			r.Body = ioutil.NopCloser(body)
-			r.ContentLength = int64(b.Len())
-		case *strings.Reader:
-			r.Body = ioutil.NopCloser(body)
-			r.ContentLength = int64(b.Len())
-		default:
-			r.Body = ioutil.NopCloser(body)
-		}
-	}
-
-	return &r, nil
+	return http.NewRequest(method, path, body)
 }

estransport/logger.go
@@ -70,7 +70,12 @@ func (l *TextLogger) LogRoundTrip(req *http.Request, res *http.Response, err err
 	)
 	if l.RequestBodyEnabled() && req != nil && req.Body != nil && req.Body != http.NoBody {
 		var buf bytes.Buffer
-		buf.ReadFrom(req.Body)
+		if req.GetBody != nil {
+			b, _ := req.GetBody()
+			buf.ReadFrom(b)
+		} else {
+			buf.ReadFrom(req.Body)
+		}
 		logBodyAsText(l.Output, &buf, ">")
 	}
 	if l.ResponseBodyEnabled() && res != nil && res.Body != nil && res.Body != http.NoBody {
@@ -130,7 +135,12 @@ func (l *ColorLogger) LogRoundTrip(req *http.Request, res *http.Response, err er
 
 	if l.RequestBodyEnabled() && req != nil && req.Body != nil && req.Body != http.NoBody {
 		var buf bytes.Buffer
-		buf.ReadFrom(req.Body)
+		if req.GetBody != nil {
+			b, _ := req.GetBody()
+			buf.ReadFrom(b)
+		} else {
+			buf.ReadFrom(req.Body)
+		}
 		fmt.Fprint(l.Output, "\x1b[2m")
 		logBodyAsText(l.Output, &buf, "       »")
 		fmt.Fprint(l.Output, "\x1b[0m")
@@ -207,7 +217,12 @@ func (l *CurlLogger) LogRoundTrip(req *http.Request, res *http.Response, err err
 
 	if req != nil && req.Body != nil && req.Body != http.NoBody {
 		var buf bytes.Buffer
-		buf.ReadFrom(req.Body)
+		if req.GetBody != nil {
+			b, _ := req.GetBody()
+			buf.ReadFrom(b)
+		} else {
+			buf.ReadFrom(req.Body)
+		}
 
 		b.Grow(buf.Len())
 		b.WriteString(" -d \\\n'")
@@ -310,7 +325,12 @@ func (l *JSONLogger) LogRoundTrip(req *http.Request, res *http.Response, err err
 	appendQuote(req.Method)
 	if l.RequestBodyEnabled() && req != nil && req.Body != nil && req.Body != http.NoBody {
 		var buf bytes.Buffer
-		buf.ReadFrom(req.Body)
+		if req.GetBody != nil {
+			b, _ := req.GetBody()
+			buf.ReadFrom(b)
+		} else {
+			buf.ReadFrom(req.Body)
+		}
 
 		b.Grow(buf.Len() + 8)
 		b.WriteString(`,"body":`)

commit f4e764f18a3a5a154b0643d8055c83fd58d90b0a

Transport: Fix error when request body was drained during retry
The changes introduced in 7f37b7b caused a bug: when the request has been retried,
the body was already drained, and therefore wasn't sent.

This patch fixes the bug by defining the `GetBody()` method for the request.

Closes #92

estransport/estransport.go
@@ -116,13 +116,25 @@ func (c *Client) Perform(req *http.Request) (*http.Response, error) {
 	var (
 		res *http.Response
 		err error
-
-		dupReqBodyForLog io.ReadCloser
 	)
 
 	// Update request
 	//
 	c.setReqUserAgent(req)
+
+	if req.Body != nil && req.Body != http.NoBody && req.GetBody == nil {
+		if !c.disableRetry || (c.logger != nil && c.logger.RequestBodyEnabled()) {
+			var buf bytes.Buffer
+			buf.ReadFrom(req.Body)
+			req.GetBody = func() (io.ReadCloser, error) {
+				r := buf
+				return ioutil.NopCloser(&r), nil
+			}
+			if req.Body, err = req.GetBody(); err != nil {
+				return nil, fmt.Errorf("cannot get request body: %s", err)
+			}
+		}
+	}
 
 	for i := 1; i <= c.maxRetries; i++ {
 		var (
@@ -143,12 +155,12 @@ func (c *Client) Perform(req *http.Request) (*http.Response, error) {
 		c.setReqURL(nodeURL, req)
 		c.setReqAuth(nodeURL, req)
 
-		// Duplicate request body for logger
-		//
-		if c.logger != nil && c.logger.RequestBodyEnabled() {
-			if req.Body != nil && req.Body != http.NoBody {
-				dupReqBodyForLog, req.Body, _ = duplicateBody(req.Body)
+		if !c.disableRetry && i > 1 && req.Body != nil && req.Body != http.NoBody {
+			body, err := req.GetBody()
+			if err != nil {
+				return nil, fmt.Errorf("cannot get request body: %s", err)
 			}
+			req.Body = body
 		}
 
 		// Set up time measures and execute the request
@@ -160,7 +172,10 @@ func (c *Client) Perform(req *http.Request) (*http.Response, error) {
 		// Log request and response
 		//
 		if c.logger != nil {
-			c.logRoundTrip(req, res, dupReqBodyForLog, err, start, dur)
+			if c.logger.RequestBodyEnabled() && req.Body != nil && req.Body != http.NoBody {
+				req.Body, _ = req.GetBody()
+			}
+			c.logRoundTrip(req, res, err, start, dur)
 		}
 
 		// Retry only on network errors, but don't retry on timeout errors, unless configured
@@ -259,7 +274,6 @@ func (c *Client) setReqUserAgent(req *http.Request) *http.Request {
 func (c *Client) logRoundTrip(
 	req *http.Request,
 	res *http.Response,
-	reqBody io.Reader,
 	err error,
 	start time.Time,
 	dur time.Duration,
@@ -267,11 +281,6 @@ func (c *Client) logRoundTrip(
 	var dupRes http.Response
 	if res != nil {
 		dupRes = *res
-	}
-	if c.logger.RequestBodyEnabled() {
-		if req.Body != nil && req.Body != http.NoBody {
-			req.Body = ioutil.NopCloser(reqBody)
-		}
 	}
 	if c.logger.ResponseBodyEnabled() {
 		if res != nil && res.Body != nil && res.Body != http.NoBody {

estransport/estransport_internal_test.go
@@ -4,6 +4,7 @@ package estransport
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -336,6 +337,40 @@ func TestTransportPerformRetries(t *testing.T) {
 
 		if i != numReqs {
 			t.Errorf("Unexpected number of requests, want=%d, got=%d", numReqs, i)
+		}
+	})
+
+	t.Run("Reset request body during retry", func(t *testing.T) {
+		var bodies []string
+		u, _ := url.Parse("https://foo.com/bar")
+		tp := New(Config{
+			URLs: []*url.URL{u},
+			Transport: &mockTransp{
+				RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+					body, err := ioutil.ReadAll(req.Body)
+					if err != nil {
+						panic(err)
+					}
+					bodies = append(bodies, string(body))
+					return &http.Response{Status: "MOCK", StatusCode: 502}, nil
+				},
+			}},
+		)
+
+		req, _ := http.NewRequest("POST", "/abc", strings.NewReader("FOOBAR"))
+		res, err := tp.Perform(req)
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+		_ = res
+
+		if n := len(bodies); n != 3 {
+			t.Fatalf("expected 3 requests, got %d", n)
+		}
+		for i, body := range bodies {
+			if body != "FOOBAR" {
+				t.Fatalf("request %d body: expected %q, got %q", i, "FOOBAR", body)
+			}
 		}
 	})
 