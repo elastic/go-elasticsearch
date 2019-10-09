bae830f81d97c5e1869fc2a0d8e60c1135883f63
Karel Minarik <karel.minarik@gmail.com>
2019-10-09 11:58:09 +0200

    Generator: Tests: URL escape string parameters for ID fields
    Fixes failures such as https://clients-ci.elastic.co/job/elastic+go-elasticsearch+master/ELASTICSEARCH_VERSION=8.0.0-SNAPSHOT,GO_VERSION=1-alpine,TEST_SUITE=xpack,label=linux/lastCompletedBuild/testReport/(root)/command-line-arguments/TestXPack_TransformTransformsCrud_Test_Invalid_Transform_Id/
    
    (cherry picked from commit bbddf24d4973e9e92080bd27f77b50e635a1c0f6)

--- internal/cmd/generate/commands/gentests/generator.go
+++ internal/cmd/generate/commands/gentests/generator.go
@@ -199,10 +199,11 @@ func (g *Generator) genFileHeader() {
 import (
 	encjson "encoding/json"
 	encyaml "gopkg.in/yaml.v2"
+	"fmt"
 	"context"
 	"crypto/tls"
-	"os"
 	"net/url"
+	"os"
 	"testing"
 	"time"
 
@@ -213,10 +214,11 @@ import (
 
 var (
 	// Prevent compilation errors for unused packages
+	_ = fmt.Printf
 	_ = encjson.NewDecoder
 	_ = encyaml.NewDecoder
 	_ = tls.Certificate{}
-	_ = fmt.Printf
+	_ = url.QueryEscape
 )` + "\n")
 }
 
@@ -966,7 +968,12 @@ func (g *Generator) genAction(a Action, skipBody ...bool) {
 						}
 						value = fmt.Sprintf("%d", dur.Nanoseconds())
 					default:
-						value = fmt.Sprintf("%q", v)
+						if strings.HasSuffix(k, "ID") {
+							value = fmt.Sprintf("url.QueryEscape(%q)", v)
+						} else {
+							value = fmt.Sprintf("%q", v)
+						}
+
 					}
 				}
 				g.w(value)
