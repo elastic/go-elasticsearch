0f8560dacfbbf6bc778de58fb0fa7f4189911c5b
Karel Minarik <karel.minarik@gmail.com>
2019-10-18 12:59:55 +0200

    WIP > ConnectionPool, add parallel benchmarks
    Measure the connection pool operations with runtime.NumCPU() * {1, 10/100/1000} goroutines.

--- estransport/connection_benchmark_test.go
+++ estransport/connection_benchmark_test.go
@@ -7,54 +7,144 @@
 package estransport
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"net/url"
 	"testing"
+
+	_ "net/http/pprof"
 )
+
+func init() {
+	go func() { log.Fatalln(http.ListenAndServe("localhost:6060", nil)) }()
+}
 
 func BenchmarkRoundRobinConnectionPool(b *testing.B) {
 	b.ReportAllocs()
 
-	b.Run("Next()", func(b *testing.B) {
-		pool := newRoundRobinConnectionPool(
-			&url.URL{Scheme: "http", Host: "foo1"},
-			&url.URL{Scheme: "http", Host: "foo2"})
+	var urls []*url.URL
+	for i := 0; i < 1000; i++ {
+		urls = append(urls, &url.URL{Scheme: "http", Host: fmt.Sprintf("foo%d", i)})
+	}
 
-		for i := 0; i < b.N; i++ {
-			_, err := pool.Next()
-			if err != nil {
-				b.Errorf("Unexpected error: %v", err)
+	b.Run("Next()", func(b *testing.B) {
+		pool := newRoundRobinConnectionPool(urls...)
+
+		b.Run("Single    ", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, err := pool.Next()
+				if err != nil {
+					b.Errorf("Unexpected error: %v", err)
+				}
 			}
-		}
+		})
+
+		b.Run("Parallel (100)", func(b *testing.B) {
+			b.SetParallelism(100)
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					_, err := pool.Next()
+					if err != nil {
+						b.Errorf("Unexpected error: %v", err)
+					}
+				}
+			})
+		})
+
+		b.Run("Parallel (1000)", func(b *testing.B) {
+			b.SetParallelism(1000)
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					_, err := pool.Next()
+					if err != nil {
+						b.Errorf("Unexpected error: %v", err)
+					}
+				}
+			})
+		})
 	})
 
 	b.Run("Remove()", func(b *testing.B) {
-		pool := newRoundRobinConnectionPool(
-			&url.URL{Scheme: "http", Host: "foo1"},
-			&url.URL{Scheme: "http", Host: "foo2"})
+		pool := newRoundRobinConnectionPool(urls...)
 
-		c, _ := pool.Next()
+		b.Run("Single    ", func(b *testing.B) {
+			c, _ := pool.Next()
 
-		for i := 0; i < b.N; i++ {
-			if err := pool.Remove(c); err != nil {
-				b.Errorf("Unexpected error: %v", err)
+			for i := 0; i < b.N; i++ {
+				if err := pool.Remove(c); err != nil {
+					b.Errorf("Unexpected error: %v", err)
+				}
 			}
+		})
 
-			pool.live = append(pool.live, c)
-		}
+		b.Run("Parallel (10)", func(b *testing.B) {
+			b.SetParallelism(10)
+			b.RunParallel(func(pb *testing.PB) {
+				c, _ := pool.Next()
+
+				for pb.Next() {
+					if err := pool.Remove(c); err != nil {
+						b.Errorf("Unexpected error: %v", err)
+					}
+				}
+			})
+		})
+
+		b.Run("Parallel (100)", func(b *testing.B) {
+			b.SetParallelism(100)
+			b.RunParallel(func(pb *testing.PB) {
+				c, _ := pool.Next()
+
+				for pb.Next() {
+					if err := pool.Remove(c); err != nil {
+						b.Errorf("Unexpected error: %v", err)
+					}
+				}
+			})
+		})
 	})
 
 	b.Run("Resurrect()", func(b *testing.B) {
-		pool := newRoundRobinConnectionPool(
-			&url.URL{Scheme: "http", Host: "foo1"},
-			&url.URL{Scheme: "http", Host: "foo2"})
+		pool := newRoundRobinConnectionPool(urls...)
 
-		c, _ := pool.Next()
-		pool.Remove(c)
+		b.Run("Single       ", func(b *testing.B) {
+			c, _ := pool.Next()
+			pool.Remove(c)
 
-		for i := 0; i < b.N; i++ {
-			if err := pool.Resurrect(c); err != nil {
-				b.Errorf("Unexpected error: %v", err)
+			for i := 0; i < b.N; i++ {
+				if err := pool.Resurrect(c); err != nil {
+					b.Errorf("Unexpected error: %v", err)
+				}
 			}
-		}
+		})
+
+		b.Run("Parallel (10)", func(b *testing.B) {
+			b.SetParallelism(10)
+			b.RunParallel(func(pb *testing.PB) {
+				c, _ := pool.Next()
+				pool.Remove(c)
+
+				for pb.Next() {
+					if err := pool.Resurrect(c); err != nil {
+						b.Errorf("Unexpected error: %v", err)
+					}
+				}
+			})
+		})
+
+		b.Run("Parallel (100)", func(b *testing.B) {
+			b.SetParallelism(100)
+			b.RunParallel(func(pb *testing.PB) {
+				c, _ := pool.Next()
+				pool.Remove(c)
+
+				for pb.Next() {
+					if err := pool.Resurrect(c); err != nil {
+						b.Errorf("Unexpected error: %v", err)
+					}
+				}
+			})
+		})
 	})
 }
