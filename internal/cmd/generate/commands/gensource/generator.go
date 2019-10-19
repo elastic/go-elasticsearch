5905135fa03eaf7fb0daa0c9676921c066a0b9a2
Karel Minarik <karel.minarik@gmail.com>
2019-10-19 12:35:17 +0200

    Generator: API: Fix the Apache 2 license header
    (cherry picked from commit a322a6dab43062ab469672184664cacaf391b93f)

--- internal/cmd/generate/commands/gensource/generator.go
+++ internal/cmd/generate/commands/gensource/generator.go
@@ -77,7 +77,7 @@ func (g *Generator) w(s string) {
 
 func (g *Generator) genHeader() {
 	g.w(`// Licensed to Elasticsearch B.V under one or more agreements.
-// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.` + "\n")
 	g.w("//\n// Code generated")
 	if EsVersion != "" {
