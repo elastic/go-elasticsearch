3e89470370aba4cea6558813da0e80916e82010c
Karel Minarik <karel.minarik@gmail.com>
2019-10-20 07:57:22 +0200

    API: Fix typo in the license headers

--- esapi/api.indices.exists_alias.go
+++ esapi/api.indices.exists_alias.go
@@ -1,5 +1,5 @@
 // Licensed to Elasticsearch B.V under one or more agreements.
-// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 //
 // Code generated from specification version 8.0.0: DO NOT EDIT
