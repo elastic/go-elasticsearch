b373d61d9a5054747d69f31ac1db04518673d867
Karel Minarik <karel.minarik@gmail.com>
2020-02-15 11:12:21 +0100

    WIP > Improve the BulkIndexer code/config and add a worker.flush() implementation

--- esutil/bulk_indexer.go
+++ esutil/bulk_indexer.go
@@ -16,41 +16,40 @@ import (
 // BulkIndexer represents a parallel, asynchronous indexer.
 //
 type BulkIndexer interface {
-	// Add adds an item to the indexer. It is asynchronous, and returns an error
-	// only when the item cannot be added. Use the OnSuccess and OnFailure
-	// callbacks to get the operation result per item.
-	Add(item BulkIndexerItem) error
+	// Add adds an item to the indexer. It returns an error when the item cannot be added.
+	// Use the OnSuccess and OnFailure callbacks to get the operation result for the item.
+	// You must call the Wait() method to allow the indexer finish the indexing.
+	// It is safe for concurrent use, but all goroutines must finish before the call to Wait().
+	Add(context.Context, BulkIndexerItem) error
 
 	// Wait blocks until all added items are indexed.
-	Wait(ctx context.Context) error
+	Wait(context.Context) error
 
-	NumAdded() uint   // NumAdded returns the number of items added to the indexer.
-	NumFailed() uint  // NumFailed returns the number of failed items.
-	NumCreated() uint // NumCreated returns the number of succesfull "create" operations.
-	NumUpdated() uint // NumUpdated returns the number of succesfull "update" operations.
-	NumDeleted() uint // NumDeleted returns the number of succesfull "delete" operations.
+	NumAdded() int   // NumAdded returns the number of items added to the indexer.
+	NumFailed() int  // NumFailed returns the number of failed items.
+	NumIndexed() int // NumCreated returns the number of succesfull "index" operations.
+	NumCreated() int // NumCreated returns the number of succesfull "create" operations.
+	NumUpdated() int // NumUpdated returns the number of succesfull "update" operations.
+	NumDeleted() int // NumDeleted returns the number of succesfull "delete" operations.
 }
 
-// bulkIndexer is a parallel, asynchronous indexer.
-//
 type bulkIndexer struct {
-	mu    sync.Mutex
-	wg    sync.WaitGroup
-	queue chan BulkIndexerItem
+	mu      sync.Mutex
+	wg      sync.WaitGroup
+	queue   chan BulkIndexerItem
+	workers []*worker
 
 	client *elasticsearch.Client
 
 	numWorkers          int
-	flushThresholdItems uint
-	flushThresholdBytes uint
+	flushThresholdBytes int
 
-	numAdded   uint
-	numCreated uint
-	numUpdated uint
-	numDeleted uint
-	numFailed  uint
-
-	// ?: Cancellation, timeouts? context.WithCancel() / context.WithTimeout()?
+	numAdded   int
+	numFailed  int
+	numIndexed int
+	numCreated int
+	numUpdated int
+	numDeleted int
 }
 
 // BulkIndexerConfig represents configuration of the bulk indexer.
@@ -58,44 +57,11 @@ type bulkIndexer struct {
 type BulkIndexerConfig struct {
 	Client *elasticsearch.Client // The Elasticsearch client.
 
-	NumWorkers          int  // The number of workers. Defaults to runtime.NumCPU().
-	FlushThresholdItems uint // The flush threshold for items. Defaults to 1000.
-	FlushThresholdBytes uint // The flush threshold in bytes. Defaults 5MB.
-}
+	NumWorkers          int // The number of workers. Defaults to runtime.NumCPU().
+	FlushThresholdBytes int // The flush threshold in bytes. Defaults to 5MB.
 
-// NewBulkIndexer creates a new bulk indexer.
-//
-func NewBulkIndexer(cfg BulkIndexerConfig) (BulkIndexer, error) {
-	if cfg.NumWorkers == 0 {
-		cfg.NumWorkers = runtime.NumCPU()
-	}
-
-	if cfg.FlushThresholdItems == 0 {
-		cfg.FlushThresholdItems = 1000
-	}
-
-	if cfg.FlushThresholdBytes == 0 {
-		cfg.FlushThresholdBytes = 5e+6
-	}
-
-	bi := bulkIndexer{
-		numWorkers:          cfg.NumWorkers,
-		flushThresholdItems: cfg.FlushThresholdItems,
-		flushThresholdBytes: cfg.FlushThresholdBytes,
-	}
-
-	bi.init()
-
-	return &bi, nil
-}
-
-// BulkIndexerItem represents an indexer item.
-//
-type BulkIndexerItem struct {
-	Index  string
-	Action string
-	Body   io.Reader
-
+	// Parameters of the Bulk API.
+	Index               string
 	ErrorTrace          bool
 	FilterPath          []string
 	Header              http.Header
@@ -109,6 +75,37 @@ type BulkIndexerItem struct {
 	SourceIncludes      []string
 	Timeout             time.Duration
 	WaitForActiveShards string
+}
+
+// NewBulkIndexer creates a new bulk indexer.
+//
+func NewBulkIndexer(cfg BulkIndexerConfig) (BulkIndexer, error) {
+	if cfg.NumWorkers == 0 {
+		cfg.NumWorkers = runtime.NumCPU()
+	}
+
+	if cfg.FlushThresholdBytes == 0 {
+		cfg.FlushThresholdBytes = 5e+6
+	}
+
+	bi := bulkIndexer{
+		numWorkers:          cfg.NumWorkers,
+		flushThresholdBytes: cfg.FlushThresholdBytes,
+	}
+
+	bi.init()
+
+	return &bi, nil
+}
+
+// BulkIndexerItem represents an indexer item.
+//
+type BulkIndexerItem struct {
+	Index           string
+	Action          string
+	DocumentID      string
+	Body            io.Reader
+	RetryOnConflict *int
 
 	Meta interface{} // To use eg. in OnSuccess/OnFailure callbacks
 
@@ -116,36 +113,63 @@ type BulkIndexerItem struct {
 	OnFailure func(item BulkIndexerItem, req interface{}, res interface{}, err error) // Per item
 }
 
-func (bi *bulkIndexer) Add(item BulkIndexerItem) error {
+func (bi *bulkIndexer) Add(ctx context.Context, item BulkIndexerItem) error {
 	bi.mu.Lock()
 	defer bi.mu.Unlock()
 
 	bi.numAdded++
-	bi.queue <- item
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case bi.queue <- item:
+	}
 
 	return nil
 }
 
 func (bi *bulkIndexer) Wait(ctx context.Context) error {
 	close(bi.queue)
-	bi.wg.Wait()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		bi.wg.Wait()
+	}
+
+	fmt.Println("==> Flushing remaining buffers...")
+	for _, w := range bi.workers {
+		if w.buf.Len() > 0 {
+			if err := w.flush(); err != nil {
+				return fmt.Errorf("flush: %s", err)
+			}
+		}
+	}
+
 	return nil
 }
 
-func (bi *bulkIndexer) NumAdded() uint   { return bi.numAdded }
-func (bi *bulkIndexer) NumFailed() uint  { return bi.numFailed }
-func (bi *bulkIndexer) NumCreated() uint { return bi.numCreated }
-func (bi *bulkIndexer) NumUpdated() uint { return bi.numUpdated }
-func (bi *bulkIndexer) NumDeleted() uint { return bi.numDeleted }
+func (bi *bulkIndexer) NumAdded() int   { bi.mu.Lock(); defer bi.mu.Unlock(); return bi.numAdded }
+func (bi *bulkIndexer) NumFailed() int  { bi.mu.Lock(); defer bi.mu.Unlock(); return bi.numFailed }
+func (bi *bulkIndexer) NumIndexed() int { bi.mu.Lock(); defer bi.mu.Unlock(); return bi.numIndexed }
+func (bi *bulkIndexer) NumCreated() int { bi.mu.Lock(); defer bi.mu.Unlock(); return bi.numCreated }
+func (bi *bulkIndexer) NumUpdated() int { bi.mu.Lock(); defer bi.mu.Unlock(); return bi.numUpdated }
+func (bi *bulkIndexer) NumDeleted() int { bi.mu.Lock(); defer bi.mu.Unlock(); return bi.numDeleted }
 
 // init initializes the bulk indexer.
 //
 func (bi *bulkIndexer) init() {
 	bi.queue = make(chan BulkIndexerItem, bi.numWorkers)
 
-	for i := 0; i < bi.numWorkers; i++ {
-		w := worker{ch: bi.queue, wg: &bi.wg, buf: bytes.NewBuffer(make([]byte, 0, bi.flushThresholdBytes))}
+	for i := 1; i <= bi.numWorkers; i++ {
+		w := worker{
+			id:  i,
+			ch:  bi.queue,
+			wg:  &bi.wg,
+			buf: bytes.NewBuffer(make([]byte, 0, bi.flushThresholdBytes)),
+			byt: bi.flushThresholdBytes}
 		w.run()
+		bi.workers = append(bi.workers, &w)
 	}
 	bi.wg.Add(bi.numWorkers)
 }
@@ -153,20 +177,72 @@ func (bi *bulkIndexer) init() {
 // worker represents an indexer worker.
 //
 type worker struct {
-	ch  <-chan BulkIndexerItem
-	wg  *sync.WaitGroup
-	buf *bytes.Buffer
+	id    int
+	ch    <-chan BulkIndexerItem
+	wg    *sync.WaitGroup
+	buf   *bytes.Buffer
+	byt   int
+	items []BulkIndexerItem
+
+	isFlushing bool
 }
 
 // run launches the worker goroutine.
 //
 func (w *worker) run() {
 	go func() {
-		fmt.Println("Started worker")
+		fmt.Printf("--> [worker-%03d] Started\n", w.id)
 		defer w.wg.Done()
 
 		for item := range w.ch {
-			fmt.Println("Got item", item)
+			fmt.Printf(">>> [worker-%03d] Got %s:%s\n", w.id, item.Action, item.DocumentID)
+			w.items = append(w.items, item)
+
+			if item.Body != nil {
+				if _, err := w.buf.ReadFrom(item.Body); err != nil {
+					if item.OnFailure != nil {
+						item.OnFailure(item, nil, nil, err)
+					}
+					continue
+				}
+				w.buf.WriteRune(',')
+			}
+
+			if w.buf.Len() >= w.byt {
+				w.flush()
+				w.items = w.items[:0]
+			}
 		}
 	}()
+}
+
+// flush writes out the buffer.
+func (w *worker) flush() error {
+	if w.isFlushing {
+		fmt.Printf(">>> [worker-%03d] Already flushing\n", w.id)
+		return nil
+	}
+	w.isFlushing = true
+
+	fmt.Printf(">>> [worker-%03d] FLUSH BUFFER: %s\n", w.id, w.buf.String())
+
+	var err error
+	if err == nil {
+		for _, item := range w.items {
+			if item.OnSuccess != nil {
+				item.OnSuccess(item, nil, nil)
+			}
+		}
+	} else {
+		for _, item := range w.items {
+			if item.OnFailure != nil {
+				item.OnFailure(item, nil, nil, fmt.Errorf("flush: %s", err))
+			}
+		}
+	}
+
+	w.buf.Reset()
+	w.isFlushing = false
+
+	return err
 }
--- esutil/bulk_indexer_internal_test.go
+++ esutil/bulk_indexer_internal_test.go
@@ -3,27 +3,74 @@
 package esutil
 
 import (
+	"context"
 	"fmt"
+	"strconv"
+	"strings"
+	"sync"
 	"testing"
+	"time"
 )
 
 // TODO(karmi): Benchmark in _examples with the Enron dataset?
 // TODO(karmi): Benchmark in _examples with the "Geopoint" and "HTTP Logs" Rally tracks.
 
 func TestBulkIndexer(t *testing.T) {
-	t.Run("Default", func(t *testing.T) {
-		bi, _ := NewBulkIndexer(BulkIndexerConfig{})
-		if err := bi.Add(BulkIndexerItem{Action: "index"}); err != nil {
-			t.Fatalf("Unexpected error: %s", err)
-		}
-		fmt.Println("NumAdded:", bi.NumAdded(), "NumFailed:", bi.NumFailed())
+	t.Run("Basic", func(t *testing.T) {
+		var wg sync.WaitGroup
+		bi, _ := NewBulkIndexer(BulkIndexerConfig{NumWorkers: 1, FlushThresholdBytes: 7})
+		numItems := 3
 
-		if bi.NumAdded() != 1 {
+		for i := 1; i <= numItems; i++ {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				err := bi.Add(context.Background(), BulkIndexerItem{
+					Action:     "index",
+					DocumentID: strconv.Itoa(i),
+					Body:       strings.NewReader(fmt.Sprintf(`foo=%d`, i)),
+				})
+				if err != nil {
+					t.Fatalf("Unexpected error: %s", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+
+		if bi.NumAdded() != numItems {
 			t.Errorf("Unexpected NumAdded: %d", bi.NumAdded())
 		}
 
-		if err := bi.Wait(nil); err != nil {
-			t.Fatalf("Unexpected error: %s", err)
+		if bi.NumFailed() != 0 {
+			t.Errorf("Unexpected NumFailed: %d", bi.NumFailed())
+		}
+
+		if err := bi.Wait(context.Background()); err != nil {
+			t.Errorf("Unexpected error: %s", err)
+		}
+
+		fmt.Println("NumAdded:", bi.NumAdded(), "NumFailed:", bi.NumFailed())
+	})
+
+	t.Run("Add() Timeout", func(t *testing.T) {
+		bi, _ := NewBulkIndexer(BulkIndexerConfig{NumWorkers: 1})
+		ctx, _ := context.WithTimeout(context.Background(), time.Nanosecond)
+		var errs []error
+		for i := 0; i < 10; i++ {
+			errs = append(errs, bi.Add(ctx, BulkIndexerItem{Action: "index"}))
+		}
+		if err := bi.Wait(context.Background()); err != nil {
+			t.Errorf("Unexpected error: %s", err)
+		}
+
+		var gotError bool
+		for _, err := range errs {
+			if err != nil && err.Error() == "context deadline exceeded" {
+				gotError = true
+			}
+		}
+		if !gotError {
+			t.Errorf("Expected timeout error, but none in: %s", errs)
 		}
 	})
 }
