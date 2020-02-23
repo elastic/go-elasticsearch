f25d5da92c5f7bd7194b00c0276b5ae38f35746f
Karel Minarik <karel.minarik@gmail.com>
2020-02-23 16:42:19 +0100

    WIP > Add a Godoc example for BulkIndexer

---
+++ esutil/bulk_indexer_example_test.go
@@ -0,0 +1,107 @@
+// Licensed to Elasticsearch B.V. under one or more agreements.
+// Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+// +build !integration
+
+package esutil_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esutil"
+)
+
+func ExampleNewBulkIndexer() {
+	log.SetFlags(0)
+
+	// Create the Elasticsearch client
+	//
+	es, err := elasticsearch.NewClient(elasticsearch.Config{
+		// Retry on 429 TooManyRequests statuses
+		//
+		RetryOnStatus: []int{502, 503, 504, 429},
+
+		// A simple incremental backoff function
+		//
+		RetryBackoff: func(i int) time.Duration { return time.Duration(i) * 100 * time.Millisecond },
+
+		// Retry up to 5 attempts
+		//
+		MaxRetries: 5,
+	})
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	// Create the indexer
+	//
+	indexer, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Client:     es,     // The Elasticsearch client
+		Index:      "test", // The default index name
+		NumWorkers: 4,      // The number of worker goroutines (default: number of CPUs)
+		FlushBytes: 5e+6,   // The flush threshold in bytes (default: 5M)
+	})
+	if err != nil {
+		log.Fatalf("Error creating the indexer: %s", err)
+	}
+
+	// Add an item to the indexer
+	//
+	err = indexer.Add(
+		context.Background(),
+		esutil.BulkIndexerItem{
+			// Action field configures the operation to perform (index, create, delete, update)
+			Action: "index",
+
+			// DocumentID is the optional document ID
+			DocumentID: "1",
+
+			// Body is an `io.Reader` with the payload
+			Body: strings.NewReader(`{"title":"Test"}`),
+
+			// OnSuccess is the optional callback for each successful operation
+			OnSuccess: func(item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
+				fmt.Printf("[%d] %s test/%s", res.Status, res.Result, item.DocumentID)
+			},
+
+			// OnFailure is the optional callback for each failed operation
+			OnFailure: func(item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem, err error) {
+				if err != nil {
+					log.Printf("ERROR: %s", err)
+				} else {
+					log.Printf("ERROR: %s: %s", res.Error.Type, res.Error.Reason)
+				}
+			},
+		},
+	)
+	if err != nil {
+		log.Fatalf("Unexpected error: %s", err)
+	}
+
+	// Close the indexer channel and flush remaining items
+	//
+	if err := indexer.Close(context.Background()); err != nil {
+		log.Fatalf("Unexpected error: %s", err)
+	}
+
+	// Report the indexer statistics
+	//
+	stats := indexer.Stats()
+	if stats.NumFailed > 0 {
+		log.Fatalf("Indexed [%d] documents with [%d] errors", stats.NumFlushed, stats.NumFailed)
+	} else {
+		log.Printf("Successfully indexed [%d] documents", stats.NumFlushed)
+	}
+
+	// For optimal performance, consider using a third-party package for JSON decoding and HTTP transport.
+	//
+	// For more information, examples and benchmarks, see:
+	//
+	// --> https://github.com/elastic/go-elasticsearch/tree/master/_examples/bulk
+}
