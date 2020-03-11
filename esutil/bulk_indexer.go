eed1c5bcacea1a7d3724d1e1a8783a374a15e6fe
Karel Minarik <karel.minarik@gmail.com>
2020-03-11 07:45:09 +0100

    Util: Add the BulkIndexer helper
    This patch adds a bulk indexing helper compoment into the "esutil" package.
    See code annotations and the bulk_indexer_example_test.go file for information on usage.
    
    Closes #137
    
    (cherry picked from commit 56b38163d20761ff1c492c627b6b28fc16e214a4)

---
+++ esutil/bulk_indexer.go
@@ -0,0 +1,544 @@
+// Licensed to Elasticsearch B.V. under one or more agreements.
+// Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+package esutil
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"runtime"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/elastic/go-elasticsearch/v6"
+	"github.com/elastic/go-elasticsearch/v6/esapi"
+)
+
+// BulkIndexer represents a parallel, asynchronous, efficient indexer for Elasticsearch.
+//
+type BulkIndexer interface {
+	// Add adds an item to the indexer. It returns an error when the item cannot be added.
+	// Use the OnSuccess and OnFailure callbacks to get the operation result for the item.
+	//
+	// You must call the Close() method after you're done adding items.
+	//
+	// It is safe for concurrent use. When it's called from goroutines,
+	// they must finish before the call to Close, eg. using sync.WaitGroup.
+	Add(context.Context, BulkIndexerItem) error
+
+	// Close waits until all added items are flushed and closes the indexer.
+	Close(context.Context) error
+
+	// Stats returns indexer statistics.
+	Stats() BulkIndexerStats
+}
+
+// BulkIndexerConfig represents configuration of the indexer.
+//
+type BulkIndexerConfig struct {
+	NumWorkers    int           // The number of workers. Defaults to runtime.NumCPU().
+	FlushBytes    int           // The flush threshold in bytes. Defaults to 5MB.
+	FlushInterval time.Duration // The flush threshold as duration. Defaults to 30sec.
+
+	Client      *elasticsearch.Client   // The Elasticsearch client.
+	Decoder     BulkResponseJSONDecoder // A custom JSON decoder.
+	DebugLogger BulkIndexerDebugLogger  // An optional logger for debugging.
+
+	OnError      func(context.Context, error)          // Called for indexer errors.
+	OnFlushStart func(context.Context) context.Context // Called when the flush starts.
+	OnFlushEnd   func(context.Context)                 // Called when the flush ends.
+
+	// Parameters of the Bulk API.
+	Index               string
+	ErrorTrace          bool
+	FilterPath          []string
+	Header              http.Header
+	Human               bool
+	Pipeline            string
+	Pretty              bool
+	Refresh             string
+	Routing             string
+	Source              []string
+	SourceExcludes      []string
+	SourceIncludes      []string
+	Timeout             time.Duration
+	WaitForActiveShards string
+}
+
+// BulkIndexerStats represents the indexer statistics.
+//
+type BulkIndexerStats struct {
+	NumAdded    uint64
+	NumFlushed  uint64
+	NumFailed   uint64
+	NumIndexed  uint64
+	NumCreated  uint64
+	NumUpdated  uint64
+	NumDeleted  uint64
+	NumRequests uint64
+}
+
+// BulkIndexerItem represents an indexer item.
+//
+type BulkIndexerItem struct {
+	Index           string
+	Action          string
+	DocumentID      string
+	Body            io.Reader
+	RetryOnConflict *int
+
+	OnSuccess func(context.Context, BulkIndexerItem, BulkIndexerResponseItem)        // Per item
+	OnFailure func(context.Context, BulkIndexerItem, BulkIndexerResponseItem, error) // Per item
+}
+
+// BulkIndexerResponse represents the Elasticsearch response.
+//
+type BulkIndexerResponse struct {
+	Took      int                                  `json:"took"`
+	HasErrors bool                                 `json:"errors"`
+	Items     []map[string]BulkIndexerResponseItem `json:"items,omitempty"`
+}
+
+// BulkIndexerResponseItem represents the Elasticsearch response item.
+//
+type BulkIndexerResponseItem struct {
+	Index      string `json:"_index"`
+	DocumentID string `json:"_id"`
+	Version    int64  `json:"_version"`
+	Result     string `json:"result"`
+	Status     int    `json:"status"`
+	SeqNo      int64  `json:"_seq_no"`
+	PrimTerm   int64  `json:"_primary_term"`
+
+	Shards struct {
+		Total      int `json:"total"`
+		Successful int `json:"successful"`
+		Failed     int `json:"failed"`
+	} `json:"_shards"`
+
+	Error struct {
+		Type   string `json:"type"`
+		Reason string `json:"reason"`
+		Cause  struct {
+			Type   string `json:"type"`
+			Reason string `json:"reason"`
+		} `json:"caused_by"`
+	} `json:"error,omitempty"`
+}
+
+// BulkResponseJSONDecoder defines the interface for custom JSON decoders.
+//
+type BulkResponseJSONDecoder interface {
+	UnmarshalFromReader(io.Reader, *BulkIndexerResponse) error
+}
+
+// BulkIndexerDebugLogger defines the interface for a debugging logger.
+//
+type BulkIndexerDebugLogger interface {
+	Printf(string, ...interface{})
+}
+
+type bulkIndexer struct {
+	wg      sync.WaitGroup
+	queue   chan BulkIndexerItem
+	workers []*worker
+	ticker  *time.Ticker
+	done    chan bool
+	stats   *bulkIndexerStats
+
+	config BulkIndexerConfig
+}
+
+type bulkIndexerStats struct {
+	numAdded    uint64
+	numFlushed  uint64
+	numFailed   uint64
+	numIndexed  uint64
+	numCreated  uint64
+	numUpdated  uint64
+	numDeleted  uint64
+	numRequests uint64
+}
+
+// NewBulkIndexer creates a new bulk indexer.
+//
+func NewBulkIndexer(cfg BulkIndexerConfig) (BulkIndexer, error) {
+	if cfg.Client == nil {
+		cfg.Client, _ = elasticsearch.NewDefaultClient()
+	}
+
+	if cfg.Decoder == nil {
+		cfg.Decoder = defaultJSONDecoder{}
+	}
+
+	if cfg.NumWorkers == 0 {
+		cfg.NumWorkers = runtime.NumCPU()
+	}
+
+	if cfg.FlushBytes == 0 {
+		cfg.FlushBytes = 5e+6
+	}
+
+	if cfg.FlushInterval == 0 {
+		cfg.FlushInterval = 30 * time.Second
+	}
+
+	bi := bulkIndexer{
+		config: cfg,
+		done:   make(chan bool),
+		stats:  &bulkIndexerStats{},
+	}
+
+	bi.init()
+
+	return &bi, nil
+}
+
+// Add adds an item to the indexer.
+//
+// Adding an item after a call to Close() will panic.
+//
+func (bi *bulkIndexer) Add(ctx context.Context, item BulkIndexerItem) error {
+	atomic.AddUint64(&bi.stats.numAdded, 1)
+
+	select {
+	case <-ctx.Done():
+		if bi.config.OnError != nil {
+			bi.config.OnError(ctx, ctx.Err())
+		}
+		return ctx.Err()
+	case bi.queue <- item:
+	}
+
+	return nil
+}
+
+// Close stops the periodic flush, closes the indexer queue channel,
+// notifies the done channel and calls flush on all writers.
+//
+func (bi *bulkIndexer) Close(ctx context.Context) error {
+	bi.ticker.Stop()
+	close(bi.queue)
+	bi.done <- true
+
+	select {
+	case <-ctx.Done():
+		if bi.config.OnError != nil {
+			bi.config.OnError(ctx, ctx.Err())
+		}
+		return ctx.Err()
+	default:
+		bi.wg.Wait()
+	}
+
+	for _, w := range bi.workers {
+		w.mu.Lock()
+		if w.buf.Len() > 0 {
+			if err := w.flush(ctx); err != nil {
+				w.mu.Unlock()
+				if bi.config.OnError != nil {
+					bi.config.OnError(ctx, err)
+				}
+				continue
+			}
+		}
+		w.mu.Unlock()
+	}
+	return nil
+}
+
+// Stats returns indexer statistics.
+//
+func (bi *bulkIndexer) Stats() BulkIndexerStats {
+	return BulkIndexerStats{
+		NumAdded:    atomic.LoadUint64(&bi.stats.numAdded),
+		NumFlushed:  atomic.LoadUint64(&bi.stats.numFlushed),
+		NumFailed:   atomic.LoadUint64(&bi.stats.numFailed),
+		NumIndexed:  atomic.LoadUint64(&bi.stats.numIndexed),
+		NumCreated:  atomic.LoadUint64(&bi.stats.numCreated),
+		NumUpdated:  atomic.LoadUint64(&bi.stats.numUpdated),
+		NumDeleted:  atomic.LoadUint64(&bi.stats.numDeleted),
+		NumRequests: atomic.LoadUint64(&bi.stats.numRequests),
+	}
+}
+
+// init initializes the bulk indexer.
+//
+func (bi *bulkIndexer) init() {
+	bi.queue = make(chan BulkIndexerItem, bi.config.NumWorkers)
+
+	for i := 1; i <= bi.config.NumWorkers; i++ {
+		w := worker{
+			id:  i,
+			ch:  bi.queue,
+			bi:  bi,
+			buf: bytes.NewBuffer(make([]byte, 0, bi.config.FlushBytes)),
+			aux: make([]byte, 0, 512)}
+		w.run()
+		bi.workers = append(bi.workers, &w)
+	}
+	bi.wg.Add(bi.config.NumWorkers)
+
+	bi.ticker = time.NewTicker(bi.config.FlushInterval)
+	go func() {
+		ctx := context.Background()
+		for {
+			select {
+			case <-bi.done:
+				return
+			case <-bi.ticker.C:
+				if bi.config.DebugLogger != nil {
+					bi.config.DebugLogger.Printf("[indexer] Auto-flushing workers after %s\n", bi.config.FlushInterval)
+				}
+				for _, w := range bi.workers {
+					w.mu.Lock()
+					if w.buf.Len() > 0 {
+						if err := w.flush(ctx); err != nil {
+							w.mu.Unlock()
+							if bi.config.OnError != nil {
+								bi.config.OnError(ctx, err)
+							}
+							continue
+						}
+					}
+					w.mu.Unlock()
+				}
+			}
+		}
+	}()
+}
+
+// worker represents an indexer worker.
+//
+type worker struct {
+	id    int
+	ch    <-chan BulkIndexerItem
+	mu    sync.Mutex
+	bi    *bulkIndexer
+	buf   *bytes.Buffer
+	aux   []byte
+	items []BulkIndexerItem
+}
+
+// run launches the worker in a goroutine.
+//
+func (w *worker) run() {
+	go func() {
+		ctx := context.Background()
+
+		if w.bi.config.DebugLogger != nil {
+			w.bi.config.DebugLogger.Printf("[worker-%03d] Started\n", w.id)
+		}
+		defer w.bi.wg.Done()
+
+		for item := range w.ch {
+			w.mu.Lock()
+
+			if w.bi.config.DebugLogger != nil {
+				w.bi.config.DebugLogger.Printf("[worker-%03d] Received item [%s:%s]\n", w.id, item.Action, item.DocumentID)
+			}
+
+			if err := w.writeMeta(item); err != nil {
+				if item.OnFailure != nil {
+					item.OnFailure(ctx, item, BulkIndexerResponseItem{}, err)
+				}
+				atomic.AddUint64(&w.bi.stats.numFailed, 1)
+				w.mu.Unlock()
+				continue
+			}
+
+			if err := w.writeBody(item); err != nil {
+				if item.OnFailure != nil {
+					item.OnFailure(ctx, item, BulkIndexerResponseItem{}, err)
+				}
+				atomic.AddUint64(&w.bi.stats.numFailed, 1)
+				w.mu.Unlock()
+				continue
+			}
+
+			w.items = append(w.items, item)
+			if w.buf.Len() >= w.bi.config.FlushBytes {
+				if err := w.flush(ctx); err != nil {
+					w.mu.Unlock()
+					if w.bi.config.OnError != nil {
+						w.bi.config.OnError(ctx, err)
+					}
+					continue
+				}
+			}
+			w.mu.Unlock()
+		}
+	}()
+}
+
+// writeMeta formats and writes the item metadata to the buffer; it must be called under a lock.
+//
+func (w *worker) writeMeta(item BulkIndexerItem) error {
+	w.buf.WriteRune('{')
+	w.aux = strconv.AppendQuote(w.aux, item.Action)
+	w.buf.Write(w.aux)
+	w.aux = w.aux[:0]
+	w.buf.WriteRune(':')
+	w.buf.WriteRune('{')
+	if item.DocumentID != "" {
+		w.buf.WriteString(`"_id":`)
+		w.aux = strconv.AppendQuote(w.aux, item.DocumentID)
+		w.buf.Write(w.aux)
+		w.aux = w.aux[:0]
+	}
+	w.buf.WriteRune('}')
+	w.buf.WriteRune('}')
+	w.buf.WriteRune('\n')
+	return nil
+}
+
+// writeBody writes the item body to the buffer; it must be called under a lock.
+//
+func (w *worker) writeBody(item BulkIndexerItem) error {
+	if item.Body != nil {
+		if _, err := w.buf.ReadFrom(item.Body); err != nil {
+			if w.bi.config.OnError != nil {
+				w.bi.config.OnError(context.Background(), err)
+			}
+			return err
+		}
+		w.buf.WriteRune('\n')
+	}
+	return nil
+}
+
+// flush writes out the worker buffer; it must be called under a lock.
+//
+func (w *worker) flush(ctx context.Context) error {
+	if w.bi.config.OnFlushStart != nil {
+		ctx = w.bi.config.OnFlushStart(ctx)
+	}
+
+	if w.bi.config.OnFlushEnd != nil {
+		defer func() { w.bi.config.OnFlushEnd(ctx) }()
+	}
+
+	if w.buf.Len() < 1 {
+		if w.bi.config.DebugLogger != nil {
+			w.bi.config.DebugLogger.Printf("[worker-%03d] Flush: Buffer empty\n", w.id)
+		}
+		return nil
+	}
+
+	var (
+		err error
+		blk BulkIndexerResponse
+	)
+
+	defer func() {
+		w.items = w.items[:0]
+		w.buf.Reset()
+	}()
+
+	if w.bi.config.DebugLogger != nil {
+		w.bi.config.DebugLogger.Printf("[worker-%03d] Flush: %s\n", w.id, w.buf.String())
+	}
+
+	atomic.AddUint64(&w.bi.stats.numRequests, 1)
+	req := esapi.BulkRequest{
+		Index: w.bi.config.Index,
+		Body:  w.buf,
+
+		Pipeline:            w.bi.config.Pipeline,
+		Refresh:             w.bi.config.Refresh,
+		Routing:             w.bi.config.Routing,
+		Source:              w.bi.config.Source,
+		SourceExcludes:      w.bi.config.SourceExcludes,
+		SourceIncludes:      w.bi.config.SourceIncludes,
+		Timeout:             w.bi.config.Timeout,
+		WaitForActiveShards: w.bi.config.WaitForActiveShards,
+
+		Pretty:     w.bi.config.Pretty,
+		Human:      w.bi.config.Human,
+		ErrorTrace: w.bi.config.ErrorTrace,
+		FilterPath: w.bi.config.FilterPath,
+		Header:     w.bi.config.Header,
+	}
+	res, err := req.Do(ctx, w.bi.config.Client)
+	if err != nil {
+		atomic.AddUint64(&w.bi.stats.numFailed, uint64(len(w.items)))
+		if w.bi.config.OnError != nil {
+			w.bi.config.OnError(ctx, fmt.Errorf("flush: %s", err))
+		}
+		return fmt.Errorf("flush: %s", err)
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	if res.IsError() {
+		atomic.AddUint64(&w.bi.stats.numFailed, uint64(len(w.items)))
+		// TODO(karmi): Wrap error (include response struct)
+		if w.bi.config.OnError != nil {
+			w.bi.config.OnError(ctx, fmt.Errorf("flush: %s", err))
+		}
+		return fmt.Errorf("flush: %s", res.String())
+	}
+
+	if err := w.bi.config.Decoder.UnmarshalFromReader(res.Body, &blk); err != nil {
+		// TODO(karmi): Wrap error (include response struct)
+		if w.bi.config.OnError != nil {
+			w.bi.config.OnError(ctx, fmt.Errorf("flush: %s", err))
+		}
+		return fmt.Errorf("flush: error parsing response body: %s", err)
+	}
+
+	for i, blkItem := range blk.Items {
+		var (
+			item BulkIndexerItem
+			info BulkIndexerResponseItem
+			op   string
+		)
+
+		item = w.items[i]
+		// The Elasticsearch bulk response contains an array of maps like this:
+		//   [ { "index": { ... } }, { "create": { ... } }, ... ]
+		// We range over the map, to set the first key and value as "op" and "info".
+		for k, v := range blkItem {
+			op = k
+			info = v
+		}
+		if info.Error.Type != "" || info.Status > 201 {
+			atomic.AddUint64(&w.bi.stats.numFailed, 1)
+			if item.OnFailure != nil {
+				item.OnFailure(ctx, item, info, nil)
+			}
+		} else {
+			atomic.AddUint64(&w.bi.stats.numFlushed, 1)
+
+			switch op {
+			case "index":
+				atomic.AddUint64(&w.bi.stats.numIndexed, 1)
+			case "create":
+				atomic.AddUint64(&w.bi.stats.numCreated, 1)
+			case "delete":
+				atomic.AddUint64(&w.bi.stats.numDeleted, 1)
+			case "update":
+				atomic.AddUint64(&w.bi.stats.numUpdated, 1)
+			}
+
+			if item.OnSuccess != nil {
+				item.OnSuccess(ctx, item, info)
+			}
+		}
+	}
+
+	return err
+}
+
+type defaultJSONDecoder struct{}
+
+func (d defaultJSONDecoder) UnmarshalFromReader(r io.Reader, blk *BulkIndexerResponse) error {
+	return json.NewDecoder(r).Decode(blk)
+}
---
+++ esutil/bulk_indexer_integration_test.go
@@ -0,0 +1,93 @@
+// Licensed to Elasticsearch B.V. under one or more agreements.
+// Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+// +build integration
+
+package esutil_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/elastic/go-elasticsearch/v6"
+	"github.com/elastic/go-elasticsearch/v6/estransport"
+	"github.com/elastic/go-elasticsearch/v6/esutil"
+)
+
+func TestBulkIndexerIntegration(t *testing.T) {
+	t.Run("Default", func(t *testing.T) {
+		var countSuccessful uint64
+		indexName := "test-bulk-integration"
+
+		es, _ := elasticsearch.NewClient(elasticsearch.Config{
+			Logger: &estransport.ColorLogger{Output: os.Stdout},
+		})
+
+		es.Indices.Delete([]string{indexName}, es.Indices.Delete.WithIgnoreUnavailable(true))
+		es.Indices.Create(
+			indexName,
+			es.Indices.Create.WithBody(strings.NewReader(`{"settings": {"number_of_shards": 1, "number_of_replicas": 0, "refresh_interval":"5s"}}`)),
+			es.Indices.Create.WithWaitForActiveShards("1"))
+
+		bi, _ := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+			Index:  indexName,
+			Client: es,
+			// FlushBytes: 3e+6,
+		})
+
+		numItems := 100000
+		body := `{"body":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}`
+		start := time.Now().UTC()
+
+		for i := 1; i <= numItems; i++ {
+			err := bi.Add(context.Background(), esutil.BulkIndexerItem{
+				Action:     "index",
+				DocumentID: strconv.Itoa(i),
+				Body:       strings.NewReader(body),
+				OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
+					atomic.AddUint64(&countSuccessful, 1)
+				},
+			})
+			if err != nil {
+				t.Fatalf("Unexpected error: %s", err)
+			}
+		}
+
+		if err := bi.Close(context.Background()); err != nil {
+			t.Errorf("Unexpected error: %s", err)
+		}
+
+		stats := bi.Stats()
+
+		if stats.NumAdded != uint64(numItems) {
+			t.Errorf("Unexpected NumAdded: want=%d, got=%d", numItems, stats.NumAdded)
+		}
+
+		if stats.NumIndexed != uint64(numItems) {
+			t.Errorf("Unexpected NumIndexed: want=%d, got=%d", numItems, stats.NumIndexed)
+		}
+
+		if stats.NumFailed != 0 {
+			t.Errorf("Unexpected NumFailed: want=0, got=%d", stats.NumFailed)
+		}
+
+		if countSuccessful != uint64(numItems) {
+			t.Errorf("Unexpected countSuccessful: want=%d, got=%d", numItems, countSuccessful)
+		}
+
+		fmt.Printf("  Added %d documents to indexer. Succeeded: %d. Failed: %d. Requests: %d. Duration: %s (%.0f docs/sec)\n",
+			stats.NumAdded,
+			stats.NumFlushed,
+			stats.NumFailed,
+			stats.NumRequests,
+			time.Since(start).Truncate(time.Millisecond),
+			1000.0/float64(time.Since(start)/time.Millisecond)*float64(stats.NumFlushed))
+	})
+}
