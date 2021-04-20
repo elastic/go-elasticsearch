c1a02a56c77085a32c42f05a06cbe43ed3e2a13b
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2021-04-20 17:26:52 +0200

    Generator: Add IP to preserved namings

--- internal/build/utils/naming.go
+++ internal/build/utils/naming.go
@@ -2,6 +2,10 @@
 // Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+// Licensed to Elasticsearch B.V. under one or more agreements.
+// Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
 package utils
 
 import "strings"
@@ -46,6 +50,7 @@ func NameToGo(s string, api ...string) string {
 	}
 
 	acronyms := map[string]string{
+		"ip":  "IP",
 		"id":  "ID",
 		"ttl": "TTL",
 
