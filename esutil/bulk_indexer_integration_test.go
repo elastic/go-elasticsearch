33f644c8e32726359c3cc9f88ab771caf41f83d1
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2021-07-08 15:46:49 +0200

    Tranport: Util: Add integration tests using compression (#302)

--- esutil/bulk_indexer_integration_test.go
+++ esutil/bulk_indexer_integration_test.go
@@ -37,136 +37,156 @@ import (
 func TestBulkIndexerIntegration(t *testing.T) {
 	body := `{"body":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}`
 
-	t.Run("Default", func(t *testing.T) {
-		var countSuccessful uint64
-		indexName := "test-bulk-integration"
+	testCases := []struct {
+		name                       string
+		CompressRequestBodyEnabled bool
+	}{
+		{
+			name:                       "Without body compression",
+			CompressRequestBodyEnabled: false,
+		},
+		{
+			name:                       "With body compression",
+			CompressRequestBodyEnabled: true,
+		},
+	}
 
-		es, _ := elasticsearch.NewClient(elasticsearch.Config{
-			Logger: &estransport.ColorLogger{Output: os.Stdout},
-		})
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Run("Default", func(t *testing.T) {
+				var countSuccessful uint64
+				indexName := "test-bulk-integration"
 
-		es.Indices.Delete([]string{indexName}, es.Indices.Delete.WithIgnoreUnavailable(true))
-		es.Indices.Create(
-			indexName,
-			es.Indices.Create.WithBody(strings.NewReader(`{"settings": {"number_of_shards": 1, "number_of_replicas": 0, "refresh_interval":"5s"}}`)),
-			es.Indices.Create.WithWaitForActiveShards("1"))
+				es, _ := elasticsearch.NewClient(elasticsearch.Config{
+					CompressRequestBody: tt.CompressRequestBodyEnabled,
+					Logger:              &estransport.ColorLogger{Output: os.Stdout},
+				})
 
-		bi, _ := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
-			Index:  indexName,
-			Client: es,
-			// FlushBytes: 3e+6,
-		})
+				es.Indices.Delete([]string{indexName}, es.Indices.Delete.WithIgnoreUnavailable(true))
+				es.Indices.Create(
+					indexName,
+					es.Indices.Create.WithBody(strings.NewReader(`{"settings": {"number_of_shards": 1, "number_of_replicas": 0, "refresh_interval":"5s"}}`)),
+					es.Indices.Create.WithWaitForActiveShards("1"))
 
-		numItems := 100000
-		start := time.Now().UTC()
+				bi, _ := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+					Index:  indexName,
+					Client: es,
+					// FlushBytes: 3e+6,
+				})
 
-		for i := 1; i <= numItems; i++ {
-			err := bi.Add(context.Background(), esutil.BulkIndexerItem{
-				Action:     "index",
-				DocumentID: strconv.Itoa(i),
-				Body:       strings.NewReader(body),
-				OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
-					atomic.AddUint64(&countSuccessful, 1)
-				},
+				numItems := 100000
+				start := time.Now().UTC()
+
+				for i := 1; i <= numItems; i++ {
+					err := bi.Add(context.Background(), esutil.BulkIndexerItem{
+						Action:     "index",
+						DocumentID: strconv.Itoa(i),
+						Body:       strings.NewReader(body),
+						OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
+							atomic.AddUint64(&countSuccessful, 1)
+						},
+					})
+					if err != nil {
+						t.Fatalf("Unexpected error: %s", err)
+					}
+				}
+
+				if err := bi.Close(context.Background()); err != nil {
+					t.Errorf("Unexpected error: %s", err)
+				}
+
+				stats := bi.Stats()
+
+				if stats.NumAdded != uint64(numItems) {
+					t.Errorf("Unexpected NumAdded: want=%d, got=%d", numItems, stats.NumAdded)
+				}
+
+				if stats.NumIndexed != uint64(numItems) {
+					t.Errorf("Unexpected NumIndexed: want=%d, got=%d", numItems, stats.NumIndexed)
+				}
+
+				if stats.NumFailed != 0 {
+					t.Errorf("Unexpected NumFailed: want=0, got=%d", stats.NumFailed)
+				}
+
+				if countSuccessful != uint64(numItems) {
+					t.Errorf("Unexpected countSuccessful: want=%d, got=%d", numItems, countSuccessful)
+				}
+
+				fmt.Printf("  Added %d documents to indexer. Succeeded: %d. Failed: %d. Requests: %d. Duration: %s (%.0f docs/sec)\n",
+					stats.NumAdded,
+					stats.NumFlushed,
+					stats.NumFailed,
+					stats.NumRequests,
+					time.Since(start).Truncate(time.Millisecond),
+					1000.0/float64(time.Since(start)/time.Millisecond)*float64(stats.NumFlushed))
 			})
-			if err != nil {
-				t.Fatalf("Unexpected error: %s", err)
-			}
-		}
 
-		if err := bi.Close(context.Background()); err != nil {
-			t.Errorf("Unexpected error: %s", err)
-		}
+			t.Run("Multiple indices", func(t *testing.T) {
+				es, _ := elasticsearch.NewClient(elasticsearch.Config{
+					CompressRequestBody: tt.CompressRequestBodyEnabled,
+					Logger:              &estransport.ColorLogger{Output: os.Stdout},
+				})
 
-		stats := bi.Stats()
+				bi, _ := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+					Index:  "test-index-a",
+					Client: es,
+				})
 
-		if stats.NumAdded != uint64(numItems) {
-			t.Errorf("Unexpected NumAdded: want=%d, got=%d", numItems, stats.NumAdded)
-		}
+				// Default index
+				for i := 1; i <= 10; i++ {
+					err := bi.Add(context.Background(), esutil.BulkIndexerItem{
+						Action:     "index",
+						DocumentID: strconv.Itoa(i),
+						Body:       strings.NewReader(body),
+					})
+					if err != nil {
+						t.Fatalf("Unexpected error: %s", err)
+					}
+				}
 
-		if stats.NumIndexed != uint64(numItems) {
-			t.Errorf("Unexpected NumIndexed: want=%d, got=%d", numItems, stats.NumIndexed)
-		}
+				// Index 1
+				for i := 1; i <= 10; i++ {
+					err := bi.Add(context.Background(), esutil.BulkIndexerItem{
+						Action: "index",
+						Index:  "test-index-b",
+						Body:   strings.NewReader(body),
+					})
+					if err != nil {
+						t.Fatalf("Unexpected error: %s", err)
+					}
+				}
 
-		if stats.NumFailed != 0 {
-			t.Errorf("Unexpected NumFailed: want=0, got=%d", stats.NumFailed)
-		}
+				// Index 2
+				for i := 1; i <= 10; i++ {
+					err := bi.Add(context.Background(), esutil.BulkIndexerItem{
+						Action: "index",
+						Index:  "test-index-c",
+						Body:   strings.NewReader(body),
+					})
+					if err != nil {
+						t.Fatalf("Unexpected error: %s", err)
+					}
+				}
 
-		if countSuccessful != uint64(numItems) {
-			t.Errorf("Unexpected countSuccessful: want=%d, got=%d", numItems, countSuccessful)
-		}
+				if err := bi.Close(context.Background()); err != nil {
+					t.Errorf("Unexpected error: %s", err)
+				}
+				stats := bi.Stats()
 
-		fmt.Printf("  Added %d documents to indexer. Succeeded: %d. Failed: %d. Requests: %d. Duration: %s (%.0f docs/sec)\n",
-			stats.NumAdded,
-			stats.NumFlushed,
-			stats.NumFailed,
-			stats.NumRequests,
-			time.Since(start).Truncate(time.Millisecond),
-			1000.0/float64(time.Since(start)/time.Millisecond)*float64(stats.NumFlushed))
-	})
+				expectedIndexed := 10 + 10 + 10
+				if stats.NumIndexed != uint64(expectedIndexed) {
+					t.Errorf("Unexpected NumIndexed: want=%d, got=%d", expectedIndexed, stats.NumIndexed)
+				}
 
-	t.Run("Multiple indices", func(t *testing.T) {
-		es, _ := elasticsearch.NewClient(elasticsearch.Config{
-			Logger: &estransport.ColorLogger{Output: os.Stdout},
+				res, err := es.Indices.Exists([]string{"test-index-a", "test-index-b", "test-index-c"})
+				if err != nil {
+					t.Fatalf("Unexpected error: %s", err)
+				}
+				if res.StatusCode != 200 {
+					t.Errorf("Expected indices to exist, but got a [%s] response", res.Status())
+				}
+			})
 		})
-
-		bi, _ := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
-			Index:  "test-index-a",
-			Client: es,
-		})
-
-		// Default index
-		for i := 1; i <= 10; i++ {
-			err := bi.Add(context.Background(), esutil.BulkIndexerItem{
-				Action:     "index",
-				DocumentID: strconv.Itoa(i),
-				Body:       strings.NewReader(body),
-			})
-			if err != nil {
-				t.Fatalf("Unexpected error: %s", err)
-			}
-		}
-
-		// Index 1
-		for i := 1; i <= 10; i++ {
-			err := bi.Add(context.Background(), esutil.BulkIndexerItem{
-				Action: "index",
-				Index:  "test-index-b",
-				Body:   strings.NewReader(body),
-			})
-			if err != nil {
-				t.Fatalf("Unexpected error: %s", err)
-			}
-		}
-
-		// Index 2
-		for i := 1; i <= 10; i++ {
-			err := bi.Add(context.Background(), esutil.BulkIndexerItem{
-				Action: "index",
-				Index:  "test-index-c",
-				Body:   strings.NewReader(body),
-			})
-			if err != nil {
-				t.Fatalf("Unexpected error: %s", err)
-			}
-		}
-
-		if err := bi.Close(context.Background()); err != nil {
-			t.Errorf("Unexpected error: %s", err)
-		}
-		stats := bi.Stats()
-
-		expectedIndexed := 10 + 10 + 10
-		if stats.NumIndexed != uint64(expectedIndexed) {
-			t.Errorf("Unexpected NumIndexed: want=%d, got=%d", expectedIndexed, stats.NumIndexed)
-		}
-
-		res, err := es.Indices.Exists([]string{"test-index-a", "test-index-b", "test-index-c"})
-		if err != nil {
-			t.Fatalf("Unexpected error: %s", err)
-		}
-		if res.StatusCode != 200 {
-			t.Errorf("Expected indices to exist, but got a [%s] response", res.Status())
-		}
-	})
+	}
 }
