8cf8bd25e2b7f6fefe3b946f20555272bf8c3c47
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2021-07-13 11:12:00 +0200

    Backport on 7.x request body compression (#305)
    * Add option to enable gzip compression on request bodies (#288)
    
    * Add option to gzip-compress request bodies.
    
    * Add some tests for gzip-compression of request body
    
    * Rename test
    
    * Undo unrelated formatting changes introduced by automated tooling
    
    * Test should also check Content-Length
    
    * Remove extraneous struct member
    
    Co-authored-by: ben <ben@cyber.casa>
    
    * Tranport: Util: Add integration tests using compression (#302)
    
    Co-authored-by: Ben Schofield <bschofield@users.noreply.github.com>
    Co-authored-by: ben <ben@cyber.casa>

--- elasticsearch.go
+++ elasticsearch.go
@@ -79,6 +79,8 @@ type Config struct {
 	EnableRetryOnTimeout bool  // Default: false.
 	MaxRetries           int   // Default: 3.
 
+	CompressRequestBody bool // Default: false.
+
 	DiscoverNodesOnStart  bool          // Discover nodes when initializing the client. Default: false.
 	DiscoverNodesInterval time.Duration // Discover nodes periodically. Default: disabled.
 
@@ -197,6 +199,8 @@ func NewClient(cfg Config) (*Client, error) {
 		EnableRetryOnTimeout: cfg.EnableRetryOnTimeout,
 		MaxRetries:           cfg.MaxRetries,
 		RetryBackoff:         cfg.RetryBackoff,
+
+		CompressRequestBody: cfg.CompressRequestBody,
 
 		EnableMetrics:     cfg.EnableMetrics,
 		EnableDebugLogger: cfg.EnableDebugLogger,
