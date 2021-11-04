011740607a450dec13d0de5cb4f6c5947192e126
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2021-11-04 14:37:03 +0100

    CI: Update elasticsearch run script

--- .ci/run-elasticsearch.sh
+++ .ci/run-elasticsearch.sh
@@ -7,7 +7,7 @@
 # Export the TEST_SUITE variable, eg. 'free' or 'platinum' defaults to 'free'.
 # Export the NUMBER_OF_NODES variable to start more than 1 node
 
-# Version 1.5.1
+# Version 1.6.1
 # - Initial version of the run-elasticsearch.sh script
 # - Deleting the volume should not dependent on the container still running
 # - Fixed `ES_JAVA_OPTS` config
@@ -19,6 +19,8 @@
 # - Added action.destructive_requires_name=false as the default will be true in v8
 # - Added ingest.geoip.downloader.enabled=false as it causes false positives in testing
 # - Moved ELASTIC_PASSWORD and xpack.security.enabled to the base arguments for "Security On by default"
+# - Use https only when TEST_SUITE is "platinum", when "free" use http
+# - Set xpack.security.enabled=false for "free" and xpack.security.enabled=true for "platinum"
 
 script_path=$(dirname $(realpath -s $0))
 source $script_path/functions/imports.sh
@@ -33,7 +35,6 @@ cluster_name=${moniker}${suffix}
 declare -a volumes
 environment=($(cat <<-END
   --env ELASTIC_PASSWORD=$elastic_password
-  --env xpack.security.enabled=true
   --env node.name=$es_node_name
   --env cluster.name=$cluster_name
   --env cluster.initial_master_nodes=$master_node_name
@@ -45,10 +46,12 @@ environment=($(cat <<-END
   --env repositories.url.allowed_urls=http://snapshot.test*
   --env action.destructive_requires_name=false
   --env ingest.geoip.downloader.enabled=false
+  --env cluster.deprecation_indexing.enabled=false
 END
 ))
 if [[ "$TEST_SUITE" == "platinum" ]]; then
   environment+=($(cat <<-END
+    --env xpack.security.enabled=true
     --env xpack.license.self_generated.type=trial
     --env xpack.security.http.ssl.enabled=true
     --env xpack.security.http.ssl.verification_mode=certificate
@@ -66,6 +69,12 @@ END
     --volume $ssl_cert:/usr/share/elasticsearch/config/certs/testnode.crt
     --volume $ssl_key:/usr/share/elasticsearch/config/certs/testnode.key
     --volume $ssl_ca:/usr/share/elasticsearch/config/certs/ca.crt
+END
+))
+else
+  environment+=($(cat <<-END
+    --env xpack.security.enabled=false
+    --env xpack.security.http.ssl.enabled=false
 END
 ))
 fi
@@ -131,3 +140,4 @@ END
   fi
 
 done
+
