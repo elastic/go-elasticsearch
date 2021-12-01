29ede6bd26ac5c15a6a78097ee9974c9f63cc665
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2021-12-01 18:06:49 +0100

    [Backport] bulkdindexer version and routing (#373)
    * Add per item routing for bulk requests (#265)
    
    * Remove docuemnt type from bulk indexer
    
    * Util: Allow setting version metadata in Bulk Indexer
    
    * Fix version type
    
    * Update tests
    
    Co-authored-by: Mike Luu <github@munkyboy.com>
    
    * CI: Util: Add integration tests to bulkindexer for external versioning (#360)
    
    Co-authored-by: Mehran Poursadeghi <mehr.prs@gmail.com>
    Co-authored-by: Mike Luu <github@munkyboy.com>

--- esutil/bulk_indexer_integration_test.go
+++ esutil/bulk_indexer_integration_test.go
@@ -15,6 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+//go:build integration
 // +build integration
 
 package esutil_test
@@ -186,6 +187,65 @@ func TestBulkIndexerIntegration(t *testing.T) {
 				if res.StatusCode != 200 {
 					t.Errorf("Expected indices to exist, but got a [%s] response", res.Status())
 				}
+			})
+
+			t.Run("External version", func(t *testing.T) {
+				var index string = "test-index-a"
+
+				es, _ := elasticsearch.NewClient(elasticsearch.Config{
+					CompressRequestBody: tt.CompressRequestBodyEnabled,
+					Logger:              &estransport.ColorLogger{Output: os.Stdout},
+				})
+
+				es.Indices.Delete([]string{index}, es.Indices.Delete.WithIgnoreUnavailable(true))
+				es.Indices.Create(index, es.Indices.Create.WithWaitForActiveShards("1"))
+
+				bulkIndex := func(bulkIndexer esutil.BulkIndexer, baseVersion int) {
+					var countTotal int = 500
+					var countSuccessful uint64
+					for i := 0; i < countTotal; i++ {
+						version := int64(baseVersion + i)
+						item := esutil.BulkIndexerItem{
+							Action:      "index",
+							Index:       index,
+							DocumentID:  strconv.Itoa(i),
+							Body:        strings.NewReader(body),
+							Version:     &version,
+							VersionType: "external",
+							OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, item2 esutil.BulkIndexerResponseItem) {
+								if version != item2.Version &&
+									version != *item.Version &&
+									item2.Result != "created" {
+									t.Fatalf("version mismatch, expected: %d, got: %d && %d", version, item.Version, item2.Version)
+								}
+								atomic.AddUint64(&countSuccessful, 1)
+							},
+						}
+						err := bulkIndexer.Add(context.Background(), item)
+						if err != nil {
+							t.Fatal(err)
+						}
+					}
+					if err := bulkIndexer.Close(context.Background()); err != nil {
+						t.Fatal(err)
+					}
+
+					if int(countSuccessful) != countTotal {
+						t.Fatalf("Unexpected countSuccessful, expected %d, got: %d", countTotal, countSuccessful)
+					}
+				}
+
+				bi, _ := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+					Index:  index,
+					Client: es,
+				})
+				bulkIndex(bi, 500)
+
+				bi, _ = esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+					Index:  index,
+					Client: es,
+				})
+				bulkIndex(bi, 900)
 			})
 		})
 	}
