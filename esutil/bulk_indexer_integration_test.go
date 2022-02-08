25f36aec36eb848209a3d8c0cfe157b98ce92178
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2022-02-08 14:09:56 +0100

    Reformat existing Go code, add build tags

--- esutil/bulk_indexer_integration_test.go
+++ esutil/bulk_indexer_integration_test.go
@@ -30,8 +30,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/elastic/elastic-transport-go/v8/elastictransport"
+	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/elastic/go-elasticsearch/v8/esutil"
 )
 
