f1456ad2e487644ee26ee8c04162698ae19c8656
lpflpf <lipengfei634626165@163.com>
2022-04-10 23:29:29 +0800

    support retry in bulk update

--- esutil/bulk_indexer.go
+++ esutil/bulk_indexer.go
@@ -444,6 +444,13 @@ func (w *worker) writeMeta(item BulkIndexerItem) error {
 		w.buf.Write(w.aux)
 		w.aux = w.aux[:0]
 	}
+	// support update retry_on_conflict
+	if item.RetryOnConflict != nil && item.Action == "update" {
+		w.buf.WriteString(",")
+		w.aux = strconv.AppendInt(w.aux, int64(*item.RetryOnConflict), 10)
+		w.buf.Write(w.aux)
+		w.aux = w.aux[:0]
+	}
 	w.buf.WriteRune('}')
 	w.buf.WriteRune('}')
 	w.buf.WriteRune('\n')
