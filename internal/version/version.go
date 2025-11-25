b4d3488721bf866840a6ebe5a6adba39dca342c9
Matt Devy <matt.devy@elastic.co>
2025-11-25 20:43:59 +0000

    ci: add release please automation (#1065)

--- internal/version/version.go
+++ internal/version/version.go
@@ -18,4 +18,7 @@
 package version
 
 // Client returns the client version as a string.
-const Client = "8.19.1-SNAPSHOT"
+const Client = Version
+
+// Version is the version of the client.
+const Version = "8.19.1-SNAPSHOT"
